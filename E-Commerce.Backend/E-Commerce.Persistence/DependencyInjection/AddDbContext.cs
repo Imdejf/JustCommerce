c23f1b5d5ad316333fe4cb60bf460cbf1c145cdc
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-03-30 22:36:24 +0200

    Added AddDbContext

---
+++ E-Commerce.Backend/E-Commerce.Persistence/DependencyInjection/AddDbContext.cs
@@ -0,0 +1,38 @@
+﻿using E_Commerce.Persistence.DbContext;
+using E_Commerce.Shared.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace E_Commerce.Persistence.DependencyInjection
+{
+    public static partial class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddECommerceDbContext(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<ECommerceDbContext>(
+                options =>
+                {
+                    options.UseSqlServer(connectionString);
+                    options.LogTo(System.Console.WriteLine);
+                    options.UseQueryTrackingBehavior(QueryTrackingBehavior.TrackAll);
+                }
+            );
+            services.AddTrackingDisposeBehaviour<ECommerceDbContext>();
+            return services;
+        }
+
+        public static IServiceCollection AddInMemoryAuthDbContext(this IServiceCollection services, string databaseName)
+        {
+            services.AddDbContext<ECommerceDbContext>(
+                options =>
+                {
+                    options.UseInMemoryDatabase(databaseName);
+                    options.LogTo(System.Console.WriteLine);
+                    options.UseQueryTrackingBehavior(QueryTrackingBehavior.TrackAll);
+                }
+            );
+            services.AddTrackingDisposeBehaviour<ECommerceDbContext>();
+            return services;
+        }
+    }
+}
