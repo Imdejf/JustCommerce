d4401e05a972f06983263d9cf9d85ed5dfd9e490
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-03-30 13:26:45 +0200

    Added JwtAuthorization DI

---
+++ E-Commerce.Backend/E-Commerce.Shared/DependencyInjection/AddJwtAuthorization.cs
@@ -0,0 +1,64 @@
+﻿using E_Commerce.Shared.Configurations;
+using E_Commerce.Shared.Services.Implementations.JwtService;
+using E_Commerce.Shared.Services.Interfaces.JwtService;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Shared.DependencyInjection
+{
+    public static partial class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddJwtAuthorization(this IServiceCollection services, JwtServiceConfig jwtServiceConfig)
+        {
+            services.AddTransient<IJwtDecoder, JwtDecoder>();
+            services.AddTransient<IJwtValidator, JwtValidator>();
+            services.Configure<JwtServiceConfig>(c =>
+            {
+                c.Issuer = jwtServiceConfig.Issuer;
+                c.ReferralId = jwtServiceConfig.ReferralId;
+                c.ReferralUrl = jwtServiceConfig.ReferralUrl;
+                c.RsaPrivateKey = jwtServiceConfig.RsaPrivateKey;
+                c.RsaPublicKey = jwtServiceConfig.RsaPublicKey;
+                c.TokenLifetimeInMinutes = jwtServiceConfig.TokenLifetimeInMinutes;
+                c.Audience = jwtServiceConfig.Audience;
+            });
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.IncludeErrorDetails = true;
+                options.TokenValidationParameters = jwtServiceConfig.AsTokenValidationParameters();
+            });
+
+            services.AddAuthorization(options =>
+            {
+                options.InvokeHandlersAfterFailure = true;
+                options.DefaultPolicy = new AuthorizationPolicyBuilder(JwtBearerDefaults.AuthenticationScheme)
+                    .RequireAuthenticatedUser()
+                    .Build();
+            });
+            return services;
+        }
+    }
+
+    public static partial class IApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseJwtAuthorization(this IApplicationBuilder app)
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+            return app;
+        }
+    }
+}
