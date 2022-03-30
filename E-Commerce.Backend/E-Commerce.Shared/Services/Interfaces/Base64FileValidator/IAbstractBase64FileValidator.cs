736f247cd4041dc3fd47360587c667063843c671
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-03-30 13:18:39 +0200

    Added IAbstractBase64FileValidatorFileStage

---
+++ E-Commerce.Backend/E-Commerce.Shared/Services/Interfaces/Base64FileValidator/IAbstractBase64FileValidator.cs
@@ -0,0 +1,15 @@
+﻿namespace E_Commerce.Shared.Services.Interfaces.Base64FileValidator
+{
+    public interface IAbstractBase64FileValidatorFileStage<TChildValidator> where TChildValidator : IAbstractBase64FileValidatorValidationStage<TChildValidator>
+    {
+        TChildValidator OnFile(Base64File base64File);
+    }
+    public interface IAbstractBase64FileValidatorValidationStage<TChildValidator> where TChildValidator : IAbstractBase64FileValidatorValidationStage<TChildValidator>
+    {
+        TChildValidator WithExtension(params string[] extensions);
+        TChildValidator WithoutExtension(params string[] extensions);
+        TChildValidator WithMaxFileSize(double fileSizeInMb);
+        TChildValidator WithMinFileSize(double fileSizeInMb);
+        bool IsValid(out string[] errors);
+    }
+}
