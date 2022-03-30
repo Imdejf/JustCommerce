d4076e35278c74e4310c2507dabe4eb9ec2951c8
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-03-30 22:37:21 +0200

    Added ECommerceDbContext

---
+++ E-Commerce.Backend/E-Commerce.Persistence/DbContext/ECommerceDbContext.cs
@@ -0,0 +1,45 @@
+﻿using E_Commerce.Domain.Entities.Abstract;
+using E_Commerce.Shared.Services.Interfaces;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Reflection;
+
+namespace E_Commerce.Persistence.DbContext
+{
+    public sealed class ECommerceDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        private readonly ICurrentUserService _currentUserService;
+
+        public ECommerceDbContext(DbContextOptions options, ICurrentUserService currentUserService) : base(options)
+        {
+            _currentUserService = currentUserService;
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(builder);
+        }
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (EntityEntry<AuditableEntity> entry in ChangeTracker.Entries<AuditableEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedBy = _currentUserService.CurrentUser.Id.ToString();
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedBy = _currentUserService.CurrentUser.Id.ToString();
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        break;
+                }
+            }
+
+            var result = await base.SaveChangesAsync(cancellationToken);
+
+            return result;
+        }
+    }
+}
