69182226a94aa11ee11774425a186b0b8e80f3dd
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-03-30 13:18:18 +0200

    Added IJwtDecoder

---
+++ E-Commerce.Backend/E-Commerce.Shared/Services/Interfaces/JwtService/IJwtDecoder.cs
@@ -0,0 +1,9 @@
+﻿using E_Commerce.Shared.Models;
+
+namespace E_Commerce.Shared.Services.Interfaces.JwtService
+{
+    public interface IJwtDecoder
+    {
+        ApplicationUserJwtClaims Decode(string jwt);
+    }
+}
