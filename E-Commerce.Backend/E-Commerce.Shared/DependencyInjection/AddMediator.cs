28ee4660164321be926f31e07e768de075892e5e
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-03-30 13:26:55 +0200

    Added Mediator DI

---
+++ E-Commerce.Backend/E-Commerce.Shared/DependencyInjection/AddMediator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Shared.DependencyInjection
+{
+    public static partial class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMediator(this IServiceCollection services, params Assembly[] handlersAssemblies)
+        {
+            services.AddMediatR(handlersAssemblies);
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+            return services;
+        }
+    }
+}
