2f6ee087b9010b8e9aa8fec4783e54f81678debc
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-03-30 12:59:12 +0200

    Added UnauthorizedAccessException

---
+++ E-Commerce.Backend/E-Commerce.Shared/Exceptions/UnauthorizedAccessException.cs
@@ -0,0 +1,11 @@
+﻿using E_Commerce.Shared.Abstract;
+
+namespace E_Commerce.Shared.Exceptions
+{
+    public sealed class UnauthorizedAccessException : BaseAppException
+    {
+        public override int StatusCodeToRise => 401;
+        public UnauthorizedAccessException(string message) : base(message) { }
+        public UnauthorizedAccessException(Dictionary<string, string[]> errors) : base(errors) { }
+    }
+}
