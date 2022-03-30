25e4c15afe81400cf446e00e425764c31221a2d0
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-03-30 13:01:34 +0200

    Added ApplicationUserJwtClaims

---
+++ E-Commerce.Backend/E-Commerce.Shared/Models/ApplicationUserJwtClaims.cs
@@ -0,0 +1,11 @@
+﻿namespace E_Commerce.Shared.Models
+{
+    public class ApplicationUserJwtClaims
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public bool IsProfilePrivate { get; set; }
+        public int RegisterSource { get; set; }
+        public int ProfileType { get; set; }
+    }
+}
