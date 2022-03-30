bc2f961a9a12142601b97853ae3fa882a0ffd0eb
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-03-30 13:42:49 +0200

    Add UseSharedMiddlewares DI

---
+++ E-Commerce.Backend/E-Commerce.Shared/DependencyInjection/UseSharedMiddlewares.cs
@@ -0,0 +1,15 @@
+﻿using E_Commerce.Shared.Middlewares;
+using Microsoft.AspNetCore.Builder;
+
+namespace E_Commerce.Shared.DependencyInjection
+{
+    public static partial class IApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseSharedMiddlewares(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ExceptionHandlingMiddleware>();
+            app.UseMiddleware<CurrentUserBindingMiddleware>();
+            return app;
+        }
+    }
+}
