f7643a89865496370858e43510e59e49ceb970a8
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-03-30 13:02:06 +0200

    Added ILoggingTool

---
+++ E-Commerce.Backend/E-Commerce.Shared/Services/Interfaces/ILoggingTool.cs
@@ -0,0 +1,10 @@
+﻿namespace E_Commerce.Shared.Services.Interfaces
+{
+    public interface ILoggingTool
+    {
+        void LogInformation(string message);
+        void LogWarning(string message);
+        void LogError(string message);
+        void LogCritical(string message);
+    }
+}
