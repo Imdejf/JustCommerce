b50abd18bb1ba3f11c0bb96a7eb2cf0943be8bf9
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-03-30 13:01:57 +0200

    Added ICurrentUserService

---
+++ E-Commerce.Backend/E-Commerce.Shared/Services/Interfaces/ICurrentUserService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Shared.Services.Interfaces
+{
+    public interface ICurrentUserService
+    {
+        ApplicationUserJwtClaims CurrentUser { get; }
+        void SetCurrentUser(ApplicationUserJwtClaims claims);
+        void SetCurrentUser(string jwt);
+    }
+}
