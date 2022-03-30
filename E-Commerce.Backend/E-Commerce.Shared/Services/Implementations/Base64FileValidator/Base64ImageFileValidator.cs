46910f72407374471cac248fd0c4069dae9d2d33
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-03-30 13:19:15 +0200

    Added Base64ImageFileValidator

---
+++ E-Commerce.Backend/E-Commerce.Shared/Services/Implementations/Base64FileValidator/Base64ImageFileValidator.cs
@@ -0,0 +1,10 @@
+﻿using E_Commerce.Shared.Services.Interfaces.Base64FileValidator;
+
+namespace E_Commerce.Shared.Services.Implementations.Base64FileValidator
+{
+    internal sealed class Base64ImageFileValidator : AbstractBase64FileValidator<IBase64ImageFileValidatorValidationStage>,
+        IBase64ImageFileValidatorValidationStage,
+        IBase64ImageFileValidator
+    {
+    }
+}
