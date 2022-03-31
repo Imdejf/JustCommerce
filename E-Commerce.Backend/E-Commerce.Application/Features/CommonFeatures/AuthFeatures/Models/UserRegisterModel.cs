a3b907635f91afc5e8c74063db7681cae1bf9134
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-03-31 23:03:57 +0200

    Added UserRegisterModel

---
+++ E-Commerce.Backend/E-Commerce.Application/Features/CommonFeatures/AuthFeatures/Models/UserRegisterModel.cs
@@ -0,0 +1,29 @@
+﻿using E_Commerce.Domain.Enums;
+
+namespace E_Commerce.Application.Features.CommonFeatures.AuthFeatures.Models
+{
+    public sealed class UserRegisterModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public UserRegisterSource Source { get; set; }
+
+        public static UserRegisterModel CreateForExternalRegister(string email, UserRegisterSource registerSource)
+        {
+            return new(email, true, Guid.NewGuid() + "A123!", registerSource);
+        }
+        public static UserRegisterModel CreateForClassicRegister(string email, string password)
+        {
+            return new(email, false, password, UserRegisterSource.Standard);
+        }
+
+        private UserRegisterModel(string email, bool emailConfirmed, string password, UserRegisterSource registerSource)
+        {
+            Email = email;
+            EmailConfirmed = emailConfirmed;
+            Password = password;
+            Source = registerSource;
+        }
+    }
+}
