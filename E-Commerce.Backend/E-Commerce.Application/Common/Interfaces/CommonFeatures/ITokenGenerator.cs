b7b118761e48bbdc89f1849177d97906105840bf
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-03-31 23:03:26 +0200

    Added ITokenGenerator

---
+++ E-Commerce.Backend/E-Commerce.Application/Common/Interfaces/CommonFeatures/ITokenGenerator.cs
@@ -0,0 +1,10 @@
+﻿using E_Commerce.Domain.Entities;
+
+namespace E_Commerce.Application.Common.Interfaces.CommonFeatures
+{
+    public interface ITokenGenerator
+    {
+        Task<string> GenerateEmailConfirmationTokenAsync(User user, CancellationToken cancellationToken);
+        Task<string> GeneratePasswordResetTokenAsync(User user, CancellationToken cancellationToken);
+    }
+}
