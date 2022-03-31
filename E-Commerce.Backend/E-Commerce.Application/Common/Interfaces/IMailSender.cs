cf956d69a18b556dc3cb82d8e787ccb03667d2fa
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-03-31 23:03:50 +0200

    Added IMailSender

---
+++ E-Commerce.Backend/E-Commerce.Application/Common/Interfaces/IMailSender.cs
@@ -0,0 +1,8 @@
+﻿namespace E_Commerce.Application.Common.Interfaces
+{
+    public interface IMailSender
+    {
+        Task SendPasswordResetEmailAsync(string reciverEmail, string passwordResetToken, Guid userId, CancellationToken cancellationToken);
+        Task SendEmailConfirmationEmailAsync(string reciverEmail, string emailConfirmationToken, Guid userId, CancellationToken cancellationToken);
+    }
+}
