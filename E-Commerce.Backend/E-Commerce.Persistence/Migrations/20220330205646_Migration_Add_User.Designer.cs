dd92142f023da670deb4b27ef793a30b819be83c
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-03-31 23:05:07 +0200

    Create new migration Migration_Add_User

--- E-Commerce.Backend/E-Commerce.Persistence/Migrations/20220330205646_Migration_Add_User.Designer.cs
+++ E-Commerce.Backend/E-Commerce.Persistence/Migrations/20220330205646_Migration_Add_User.Designer.cs
@@ -4,6 +4,7 @@ using E_Commerce.Persistence.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace E_Commerce.Persistence.Migrations
 {
     [DbContext(typeof(ECommerceDbContext))]
-    partial class ECommerceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220330205646_Migration_Add_User")]
+    partial class Migration_Add_User
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
