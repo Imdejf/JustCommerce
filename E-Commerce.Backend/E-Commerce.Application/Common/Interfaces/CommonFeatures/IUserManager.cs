7bc7fb73ddadd320b63d6618a6d580ff568cf745
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-03-31 23:03:33 +0200

    Added IUserManager

---
+++ E-Commerce.Backend/E-Commerce.Application/Common/Interfaces/CommonFeatures/IUserManager.cs
@@ -0,0 +1,22 @@
+﻿using E_Commerce.Application.Features.CommonFeatures.AuthFeatures.Models;
+using E_Commerce.Application.Models;
+using E_Commerce.Domain.Entities;
+
+namespace E_Commerce.Application.Common.Interfaces.CommonFeatures
+{
+    public interface IUserManager
+    {
+        Task<bool> ExistsAsync(Guid id, CancellationToken cancellationToken);
+        Task SetProfileTypeAsync(Guid userId, CancellationToken cancellationToken);
+        Task SetProfileAccessAsync(Guid userId, bool isProfilePrivate, CancellationToken cancellationToken);
+        Task<IdentityActionResult> ChangePasswordAsync(User user, string oldPassword, string newPassword, CancellationToken cancellationToken);
+        Task<IdentityActionResult> LoginAsync(User user, string password, CancellationToken cancellationToken);
+        Task<bool> IsEmailTakenAsync(string email, CancellationToken cancellationToken);
+        Task<(User, IdentityActionResult)> RegisterAsync(UserRegisterModel userRegisterModel, CancellationToken cancellationToken);
+        Task<IdentityActionResult> ResetPasswordAsync(User user, string password, string passwordResetToken, CancellationToken cancellationToken);
+        Task<IdentityActionResult> ConfirmEmailAsync(User user, string emailConfirmationToken, CancellationToken cancellationToken);
+        Task RemoveAccountAsync(User user, CancellationToken cancellationToken);
+        Task<User> GetByIdAsync(Guid userId, CancellationToken cancellationToken);
+        Task<User> GetByEmailAsync(string email, CancellationToken cancellationToken);
+    }
+}
