7c9e958ede796819b654797b6da7b1e071981e9f
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-03 23:07:48 +0200

    Add RoleConfig

---
+++ JustCommerce.Backend/JustCommerce.Persistence/DataAccess/EntitiesConfig/Identity/RoleConfig.cs
@@ -0,0 +1,20 @@
+﻿using JustCommerce.Domain.Entities.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace JustCommerce.Persistence.DataAccess.EntitiesConfig.Identity
+{
+    public sealed class RoleConfig : IEntityTypeConfiguration<RoleEntity>
+    {
+        public void Configure(EntityTypeBuilder<RoleEntity> builder)
+        {
+            builder.ToTable("Role", "identity");
+
+            builder.Property(c => c.Name)
+                   .HasColumnType("varchar");
+
+            builder.Property(c => c.NormalizedName)
+                   .HasColumnType("varchar");
+        }
+    }
+}
