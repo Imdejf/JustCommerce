b1a51bc830fc99ba8b8590237ac75dc36132945d
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-03 23:07:09 +0200

    Add UserEntity

---
+++ JustCommerce.Backend/JustCommerce.Domain/Entities/Identity/UserEntity.cs
@@ -0,0 +1,14 @@
+﻿using JustCommerce.Domain.Enums;
+using Microsoft.AspNetCore.Identity;
+
+namespace JustCommerce.Domain.Entities.Identity
+{
+    public sealed class UserEntity : IdentityUser<Guid>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public UserRegisterSource RegisterSource { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
