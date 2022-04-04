c394c4fbd29ccfbee377924f2182ed36eff596b7
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-04 21:30:32 +0200

    Add Roles enum

---
+++ JustCommerce.Backend/JustCommerce.Domain/Enums/Role.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustCommerce.Domain.Enums
+{
+    public enum Roles
+    {
+        SuperVisior,
+        Manager,
+        Seller,
+        Basic
+    }
+
+    public static class RoleConstant
+    {
+        public static readonly Guid SuperVisior = Guid.NewGuid();
+        public static readonly Guid Manager = Guid.NewGuid();
+        public static readonly Guid Seller = Guid.NewGuid();
+        public static readonly Guid Basic = Guid.NewGuid();
+    }
+}
