ff41ffef0bc5346c08127590e17ec004c2263b05
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-04 21:30:18 +0200

    Add SendPasswordResetEmail

---
+++ JustCommerce.Backend/JustCommerce.Application/Features/CommonFeatures/AuthFeatures/Query/SendPasswordResetEmail.cs
@@ -0,0 +1,46 @@
+﻿using FluentValidation;
+using JustCommerce.Application.Common.Interfaces;
+using JustCommerce.Application.Common.Interfaces.CommonFeatures;
+using JustCommerce.Shared.Exceptions;
+using MediatR;
+
+namespace JustCommerce.Application.Features.CommonFeatures.AuthFeatures.Query
+{
+    public static class SendPasswordResetEmail
+    {
+        public sealed record Query(string Email) : IRequestWrapper<Unit>;
+        public sealed class Handler : IRequestHandlerWrapper<Query, Unit>
+        {
+            private readonly IUserManager _UserManager;
+            private readonly ITokenGenerator _TokenGenerator;
+            private readonly IMailSender _EmailSender;
+            public Handler(IUserManager userManager, ITokenGenerator tokenGenerator, IMailSender emailSender)
+            {
+                _UserManager = userManager;
+                _TokenGenerator = tokenGenerator;
+                _EmailSender = emailSender;
+            }
+
+            public async Task<Unit> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var currentUser = await _UserManager.GetByEmailAsync(request.Email, cancellationToken);
+                if (currentUser is null)
+                {
+                    throw new EntityNotFoundException($"User with Email {request.Email} doesn`t exists");
+                }
+
+                var emailConfiramtionToken = await _TokenGenerator.GeneratePasswordResetTokenAsync(currentUser, cancellationToken);
+                await _EmailSender.SendPasswordResetEmailAsync(currentUser.Email, emailConfiramtionToken, currentUser.Id, cancellationToken);
+
+                return Unit.Value;
+            }
+        }
+        public sealed class Validator : AbstractValidator<Query>
+        {
+            public Validator()
+            {
+                RuleFor(c => c.Email).NotEmpty().EmailAddress();
+            }
+        }
+    }
+}
