bb38cb3f2ef63ec16c72c43da7c338cfa4086679
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-04 21:29:38 +0200

    Add Register

---
+++ JustCommerce.Backend/JustCommerce.Application/Features/CommonFeatures/AuthFeatures/Command/Register.cs
@@ -0,0 +1,61 @@
+﻿using E_Commerce.Application.Common.Exceptions;
+using FluentValidation;
+using JustCommerce.Application.Common.Extension;
+using JustCommerce.Application.Common.Interfaces;
+using JustCommerce.Application.Common.Interfaces.CommonFeatures;
+using JustCommerce.Domain.Entities.Identity;
+using JustCommerce.Shared.Exceptions;
+
+namespace JustCommerce.Application.Features.CommonFeatures.AuthFeatures.Command
+{
+    public static class Register
+    {
+        public sealed record Command(string Login, string Email, string Password, string PasswordCopy, string FirstName, string Surname,
+                                     string? CompanyName, string? Nip, string Province, string Street) : IRequestWrapper<UserEntity>;
+
+        public sealed class Handler : IRequestHandlerWrapper<Command, UserEntity>
+        {
+            private readonly IUserManager _UserManager;
+            private readonly ITokenGenerator _TokenGenerator;
+            private readonly IMailSender _EmailSender;
+            public Handler(IUserManager userManager, ITokenGenerator tokenGenerator, IMailSender emailSender)
+            {
+                _UserManager = userManager;
+                _TokenGenerator = tokenGenerator;
+                _EmailSender = emailSender;
+            }
+
+            public async Task<UserEntity> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var isEmailTaken = await _UserManager.IsEmailTakenAsync(request.Email, cancellationToken);
+                if (isEmailTaken)
+                {
+                    throw new InvalidRequestException($"Email {request.Email} is already taken");
+                }
+
+                var result = await _UserManager.RegisterAsync(Models.UserRegisterModel.CreateForClassicRegister(request.Email, request.Password, request.FirstName, request.Surname), cancellationToken);
+
+                if (!result.Item2.IsSuccessful)
+                {
+                    throw new IdentityException(result.Item2);
+                }
+
+                var registeredUser = result.Item1;
+                var emailConfirmationToken = await _TokenGenerator.GenerateEmailConfirmationTokenAsync(registeredUser, cancellationToken);
+                await _EmailSender.SendEmailConfirmationEmailAsync(registeredUser.Email, emailConfirmationToken, registeredUser.Id, cancellationToken);
+
+                return registeredUser;
+            }
+        }
+        public sealed class Validator : AbstractValidator<Command>
+        {
+            public Validator()
+            {
+                RuleFor(c => c.Email).NotEmpty().EmailAddress();
+                RuleFor(c => c).Must(c => c.Password == c.PasswordCopy);
+                RuleFor(c => c.Password).Matches(RegexExtension.PasswordValidationRegex);
+                RuleFor(c => c.PasswordCopy).Matches(RegexExtension.PasswordValidationRegex);
+            }
+        }
+    }
+}
