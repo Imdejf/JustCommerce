4edcd55f3370b1106ab9c54bf9962db568ab8f0c
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-09 22:54:50 +0200

    Add ForbiddenAccessException

---
+++ JustCommerce.Backend/JustCommerce.Application/Common/Exceptions/ForbiddenAccessException.cs
@@ -0,0 +1,7 @@
+﻿namespace JustCommerce.Application.Common.Exceptions
+{
+    public class ForbiddenAccessException : Exception
+    {
+        public ForbiddenAccessException() : base() { }
+    }
+}
