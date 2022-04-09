391af0fb91a22a9ec5f21eacfe89e573f4c015ee
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-09 22:53:23 +0200

    Add basic controller for managemnet

---
+++ JustCommerce.Backend/JustCommerce.Api/Controllers/ManagementController/EmailTemplateController.cs
@@ -0,0 +1,16 @@
+﻿using JustCommerce.Shared.Abstract;
+using JustCommerce.Shared.Services.Interfaces;
+
+namespace JustCommerce.Api.Controllers.ManagementController
+{
+    [Route("/api/management")]
+    public class EmailTemplateController : BaseApiController
+    {
+        private readonly ICurrentUserService _currentUserService;
+
+        public UserController(ICurrentUserService currentUserService)
+        {
+            _currentUserService = currentUserService;
+        }
+    }
+}
---
+++ JustCommerce.Backend/JustCommerce.Api/Controllers/ManagementController/UserController.cs
@@ -0,0 +1,17 @@
+﻿using JustCommerce.Shared.Abstract;
+using JustCommerce.Shared.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JustCommerce.Api.Controllers.ManagementController
+{
+    [Route("api/management")]
+    public class UserController : BaseApiController
+    {
+        private readonly ICurrentUserService _currentUserService;
+
+        public UserController(ICurrentUserService currentUserService)
+        {
+            _currentUserService = currentUserService;
+        }
+    }
+}
