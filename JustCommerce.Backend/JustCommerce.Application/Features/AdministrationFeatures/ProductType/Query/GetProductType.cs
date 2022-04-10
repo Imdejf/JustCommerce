b6d4a1b41dda32eacf03fd7f66e0ec16b9a83df9
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-10 23:55:05 +0200

    Add GetProductType

---
+++ JustCommerce.Backend/JustCommerce.Application/Features/AdministrationFeatures/ProductType/Query/GetProductType.cs
@@ -0,0 +1,26 @@
+﻿using JustCommerce.Application.Common.DataAccess.Repository;
+using JustCommerce.Application.Common.Interfaces;
+using JustCommerce.Domain.Entities.ProductType;
+
+namespace JustCommerce.Application.Features.AdministrationFeatures.ProductType.Query
+{
+    public static class GetProductType
+    {
+        public sealed record Query : IRequestWrapper<List<ProductTypeEntity>>;
+
+        public sealed class Handler : IRequestHandlerWrapper<Query, List<ProductTypeEntity>>
+        {
+            private readonly IUnitOfWorkAdministration _unitOfWorkAdministration;
+
+            public Handler(IUnitOfWorkAdministration unitOfWorkAdministration)
+            {
+                _unitOfWorkAdministration = unitOfWorkAdministration;
+            }
+
+            public async Task<List<ProductTypeEntity>> Handle(Query request, CancellationToken cancellationToken)
+            {
+               return await _unitOfWorkAdministration.ProductType.GetAllAsync(cancellationToken);
+            }
+        }
+    }
+}
