5d5116e84b22e2e5d1609fbfbc37fc24dbc62f89
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-10 23:58:01 +0200

    Add ProductTypeConfig

---
+++ JustCommerce.Backend/JustCommerce.Persistence/DataAccess/EntitiesConfig/ProductType/ProductTypeConfig.cs
@@ -0,0 +1,42 @@
+﻿using JustCommerce.Domain.Entities.ProductType;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustCommerce.Persistence.DataAccess.EntitiesConfig.ProductType
+{
+    public sealed class ProductTypeConfig : IEntityTypeConfiguration<ProductTypeEntity>
+    {
+        public void Configure(EntityTypeBuilder<ProductTypeEntity> builder)
+        {
+            builder.ToTable("ProductType");
+
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Name)
+                   .HasColumnType("varchar")
+                   .HasMaxLength(150)
+                   .IsRequired();
+
+            builder.Property(c => c.CreatedBy)
+                   .HasColumnType("varchar")
+                   .HasMaxLength(50)
+                   .IsRequired(false);
+
+            builder.Property(c => c.CreatedDate)
+                   .HasColumnType("datetime")
+                   .IsRequired();
+
+            builder.Property(c => c.LastModifiedBy)
+                    .HasColumnType("varchar")
+                    .HasMaxLength(50);
+
+            builder.Property(c => c.LastModifiedDate)
+                   .HasColumnType("datetime");
+        }
+    }
+}
