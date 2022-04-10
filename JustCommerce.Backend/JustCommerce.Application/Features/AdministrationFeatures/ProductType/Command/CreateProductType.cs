e575a99d274e9045aecfd3091ec2fd4cf1d4d92e
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-10 23:54:45 +0200

    Add CreateProductType

---
+++ JustCommerce.Backend/JustCommerce.Application/Features/AdministrationFeatures/ProductType/Command/CreateProductType.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using JustCommerce.Application.Common.DataAccess.Repository;
+using JustCommerce.Application.Common.Interfaces;
+using JustCommerce.Domain.Entities.ProductType;
+
+namespace JustCommerce.Application.Features.AdministrationFeatures.ProductType.Command
+{
+    public static class CreateProductType
+    {
+
+        public sealed record Command(string Name) : IRequestWrapper<ProductTypeEntity>;
+
+        public sealed class Handler : IRequestHandlerWrapper<Command, ProductTypeEntity>
+        {
+            private readonly IUnitOfWorkAdministration _unitOfWorkAdministration;
+
+            public Handler(IUnitOfWorkAdministration unitOfWorkAdministration)
+            {
+                _unitOfWorkAdministration = unitOfWorkAdministration;
+            }
+
+            public async Task<ProductTypeEntity> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var productType = new ProductTypeEntity
+                {
+                    Name = request.Name,
+                };
+                
+                await _unitOfWorkAdministration.ProductType.AddAsync(productType, cancellationToken);
+                
+                await _unitOfWorkAdministration.SaveChangesAsync(cancellationToken);
+                return productType;
+            }
+        }
+        public sealed class Validator : AbstractValidator<Command>
+        {
+            public Validator()
+            {
+                RuleFor(c => c.Name).NotEmpty();
+            }
+        }
+    }
+}
