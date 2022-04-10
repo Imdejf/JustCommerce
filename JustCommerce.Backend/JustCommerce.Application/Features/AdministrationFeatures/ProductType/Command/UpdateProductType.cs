95421c70a89edeb01ceb8ad3a458bb35e08cc793
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-10 23:54:59 +0200

    Add UpdateProductType

---
+++ JustCommerce.Backend/JustCommerce.Application/Features/AdministrationFeatures/ProductType/Command/UpdateProductType.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation;
+using JustCommerce.Application.Common.DataAccess.Repository;
+using JustCommerce.Application.Common.Interfaces;
+using JustCommerce.Shared.Exceptions;
+using MediatR;
+
+namespace JustCommerce.Application.Features.AdministrationFeatures.ProductType.Command
+{
+    public static class UpdateProductType
+    {
+        public sealed record Command(Guid ProductTypeId,string Name) : IRequestWrapper<Unit>;
+
+        public sealed class Handler : IRequestHandlerWrapper<Command, Unit>
+        {
+            private readonly IUnitOfWorkAdministration _unitOfWorkAdministration;
+
+            public Handler(IUnitOfWorkAdministration unitOfWorkAdministration)
+            {
+                _unitOfWorkAdministration = unitOfWorkAdministration;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var productType = await _unitOfWorkAdministration.ProductType.GetByIdAsync(request.ProductTypeId, cancellationToken);
+
+                if(productType is null)
+                {
+                    throw new EntityNotFoundException($"Update ProductType Id:{request.ProductTypeId}");
+                }
+
+                productType.Name = request.Name;
+
+                await _unitOfWorkAdministration.SaveChangesAsync(cancellationToken);
+
+                return Unit.Value;
+
+            }
+        }
+        public sealed class Validator : AbstractValidator<Command>
+        {
+            public Validator()
+            {
+                RuleFor(c => c.ProductTypeId).NotEqual(Guid.Empty);
+                RuleFor(c => c.Name).NotEmpty();
+            }
+        }
+    }
+}
