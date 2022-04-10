fc791a21daf22b7f7ea535985b28aca6c7e3048c
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-10 23:55:35 +0200

    Add ProductTypeEntity

---
+++ JustCommerce.Backend/JustCommerce.Domain/Entities/ProductType/ProductTypeEntity.cs
@@ -0,0 +1,9 @@
+﻿using JustCommerce.Domain.Entities.Abstract;
+
+namespace JustCommerce.Domain.Entities.ProductType
+{
+    public sealed class ProductTypeEntity : AuditableEntity
+    {
+        public string Name { get; set; }
+    }
+}
