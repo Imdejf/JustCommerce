246b27838b3934a554328862860847bbbb95ef56
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-10 23:55:26 +0200

    Add DI AddApplication

---
+++ JustCommerce.Backend/JustCommerce.Application/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace JustCommerce.Application
+{
+    public static partial class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
