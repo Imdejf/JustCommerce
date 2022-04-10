4565191fd39bb185458589235c5def51717eeee4
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-10 23:54:38 +0200

    Add IUnitOfWorkManagmenet

---
+++ JustCommerce.Backend/JustCommerce.Application/Common/Interfaces/DataAccess/Repository/IUnitOfWorkManagmenet.cs
@@ -0,0 +1,7 @@
+﻿namespace JustCommerce.Application.Common.DataAccess.Repository
+{
+    public interface IUnitOfWorkManagmenet
+    {
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
