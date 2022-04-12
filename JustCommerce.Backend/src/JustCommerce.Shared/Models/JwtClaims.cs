d2076b71add27145908c0fabf9c64ef9be48cce2
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-12 22:51:36 +0200

    Add JwtClaims

---
+++ JustCommerce.Backend/src/JustCommerce.Shared/Models/JwtClaims.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace JustCommerce.Shared.Models
+{
+    public class JwtClaims
+    {
+        public Guid UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public int RegisterSource { get; set; }
+        public IDictionary<string, IEnumerable<int>> PermissionsList { get; set; }
+        public IDictionary<string, int> Permissions => PermissionsList.ToDictionary(c => c.Key.Replace("LIST**", ""), c => c.Value.Sum());
+
+        public static JwtClaims CreateFromJwtClaimsCollection(IEnumerable<Claim> claims)
+        {
+            var mockedJwtClaims = new JwtClaims();
+            return new JwtClaims
+            {
+                Email = claims.FirstOrDefault(c => c.Type == nameof(mockedJwtClaims.Email)).Value,
+                FirstName = claims.FirstOrDefault(c => c.Type == nameof(mockedJwtClaims.FirstName)).Value,
+                LastName = claims.FirstOrDefault(c => c.Type == nameof(mockedJwtClaims.LastName)).Value,
+                UserName = claims.FirstOrDefault(c => c.Type == nameof(mockedJwtClaims.UserName)).Value,
+                UserId = new Guid(claims.FirstOrDefault(c => c.Type == nameof(mockedJwtClaims.UserId)).Value),
+                PermissionsList = claims
+                    .Where(c => c.Type.EndsWith("LIST**"))
+                    .GroupBy(c => c.Type)
+                    .ToDictionary(c => c.Key, c => c.Select(c => int.Parse(c.Value)))
+            };
+        }
+    }
+}
