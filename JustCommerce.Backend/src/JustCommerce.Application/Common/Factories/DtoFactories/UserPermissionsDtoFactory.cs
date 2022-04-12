3cd8f518f149febbad23256358eeb72431f164ab
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-12 22:45:50 +0200

    Add UserPermissionsDtoFactory

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Factories/DtoFactories/UserPermissionsDtoFactory.cs
@@ -0,0 +1,16 @@
+﻿using JustCommerce.Application.Common.DTOs;
+
+namespace JustCommerce.Application.Common.Factories.DtoFactories
+{
+    public static class UserPermissionsDtoFactory
+    {
+        public static UserPermissionsDTO CreateFromData(Guid userId, IEnumerable<PermissionDTO> owned, IEnumerable<PermissionDTO> unowned)
+        {
+            return new UserPermissionsDTO
+            {
+                UserId = userId,
+                Permission = owned.Select(c => new Tuple<PermissionDTO, bool>(c, true)).Union(unowned.Select(a => new Tuple<PermissionDTO, bool>(a, false))).ToArray()
+            };
+        }
+    }
+}
