3122d0f8b1ff133d016bfc4717ebea1064349ff9
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-12 22:51:42 +0200

    Add PermissionObject

---
+++ JustCommerce.Backend/src/JustCommerce.Shared/Models/PermissionObject.cs
@@ -0,0 +1,15 @@
+﻿namespace E_Commerce.Shared.Models
+{
+    public sealed class PermissionObject
+    {
+        public string PermissionDomainName { get; set; }
+        public string PermissionFlagName { get; set; }
+        public int PermissionFlagValue { get; set; }
+        internal PermissionObject(string domainName, string flagName, int flagValue)
+        {
+            PermissionDomainName = domainName;
+            PermissionFlagName = flagName;
+            PermissionFlagValue = flagValue;
+        }
+    }
+}
