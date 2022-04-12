f5a78d03aecb73299ea60078d2fc86a9e5ae3648
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-12 22:45:55 +0200

    Add UserEntityFacotry

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Factories/EntitiesFactories/UserEntityFacotry.cs
@@ -0,0 +1,24 @@
+﻿using JustCommerce.Application.Features.CommonFeatures.AuthFeatures.Command;
+using JustCommerce.Domain.Entities.Identity;
+
+namespace JustCommerce.Application.Common.Factories.EntitiesFactories
+{
+    public static class UserEntityFacotry
+    {
+        public static UserEntity CreateFromRegisterCommand(Register.Command command)
+        {
+            return new UserEntity
+            {
+                Email = command.Email,
+                FirstName = command.FirstName,
+                LastName = command.LastName,
+                PhoneNumber = command.PhoneNumber,
+                EmailConfirmed = false,
+                RegisterSource = command.RegisterSource,
+                ShopId = command.ShopId,
+                UserName = command.Login,
+                CreatedDate = DateTime.Now,
+            };
+        }
+    }
+}
