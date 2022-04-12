2b61d8d496b9de56da7ee9c8499bd6b29a389e7f
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-12 22:47:27 +0200

    Add GetAllProfile query

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Features/ManagemenetFeatures/Permission/Query/GetAllProfile.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using JustCommerce.Application.Common.DTOs;
+using JustCommerce.Application.Common.Factories.DtoFactories;
+using JustCommerce.Application.Common.Interfaces;
+using JustCommerce.Shared.Services.Interfaces.Permission;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JustCommerce.Application.Features.ManagemenetFeatures.Permission.Query
+{
+    public static class GetAllProfile
+    {
+
+        public sealed record Query() : IRequestWrapper<IEnumerable<ProfileDTO>>;
+
+        public sealed class Handler : IRequestHandlerWrapper<Query, IEnumerable<ProfileDTO>>
+        {
+            private readonly IPermissionsMapper _permissionsMapper;
+            public Handler(IPermissionsMapper permissionsMapper)
+            {
+                _permissionsMapper = permissionsMapper;
+            }
+
+            public async Task<IEnumerable<ProfileDTO>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return _permissionsMapper.GetProfilesAsObjects().Select(c => ProfileDtoFactory.CreateFromData(c.FlagName, c.FlagValue));
+            }
+        }
+    }
+}
