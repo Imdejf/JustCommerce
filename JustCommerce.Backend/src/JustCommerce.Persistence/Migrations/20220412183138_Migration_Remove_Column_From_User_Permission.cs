f6b09430211e74d6b2d90f647e986bed73261b21
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-12 22:49:51 +0200

    Add new migration Migration_Remove_Column_From_User_Permission

---
+++ JustCommerce.Backend/src/JustCommerce.Persistence/Migrations/20220412183138_Migration_Remove_Column_From_User_Permission.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JustCommerce.Persistence.Migrations
+{
+    public partial class Migration_Remove_Column_From_User_Permission : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Id",
+                schema: "identity",
+                table: "UserPermission");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "Id",
+                schema: "identity",
+                table: "UserPermission",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}
