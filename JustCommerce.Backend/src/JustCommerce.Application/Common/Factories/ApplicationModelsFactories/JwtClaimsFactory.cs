01989345fc37c448ffe2d00c5a14d070575aa0fd
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-12 22:45:27 +0200

    Add JwtClaimsFactory

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Factories/ApplicationModelsFactories/JwtClaimsFactory.cs
@@ -0,0 +1,26 @@
+﻿using JustCommerce.Application.Common.DTOs;
+using JustCommerce.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustCommerce.Application.Common.Factories.ApplicationModelsFactories
+{
+    public static class JwtClaimsFactory
+    {
+        public static JustCommerceJwtClaims CreateFromIntranetUserDTO(UserDTO dto)
+        {
+            return new JustCommerceJwtClaims
+            {
+                Email = dto.Email,
+                FirstName = dto.FirstName,
+                LastName = dto.LastName,
+                UserName = dto.UserName,
+                PermissionsList = dto.Permissions,
+                UserId = dto.Id
+            };
+        }
+    }
+}
