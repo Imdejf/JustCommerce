77bcf9368ea38a4c5540b07c6cf51c006d1a7990
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-12 22:50:58 +0200

    Add AddPermissionStorage

---
+++ JustCommerce.Backend/src/JustCommerce.Shared/DependencyInjection/AddPermissionStorage.cs
@@ -0,0 +1,20 @@
+﻿using E_Commerce.Shared.MediatorPipelineBehaviours;
+using E_Commerce.Shared.Services.Implementations.PermissionMapper;
+using E_Commerce.Shared.Services.Interfaces.Permission;
+using JustCommerce.Shared.Services.Interfaces.Permission;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace E_Commerce.Shared.DependencyInjection
+{
+    public static class AddPermissionStorage
+    {
+        public static IServiceCollection AddPermissionsStorage(this IServiceCollection services)
+        {
+            services.AddTransient<IPermissionValidator, PermissionValidator>();
+            services.AddTransient<IPermissionsMapper, PermissionsMapper>();
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehaviour<,>));
+            return services;
+        }
+    }
+}
