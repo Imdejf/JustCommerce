b23bbc4df6582204f1deac0a4fc0371ce838f431
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-12 22:51:47 +0200

    Add ProfileObject

---
+++ JustCommerce.Backend/src/JustCommerce.Shared/Models/ProfileObject.cs
@@ -0,0 +1,8 @@
+﻿namespace E_Commerce.Shared.Models
+{
+    public sealed class ProfileObject
+    {
+        public string FlagName { get; set; }
+        public int FlagValue { get; set; }
+    }
+}
