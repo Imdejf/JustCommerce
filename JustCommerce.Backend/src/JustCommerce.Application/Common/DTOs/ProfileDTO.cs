38312850ebc4aeea3e8b4dab31da8c7c72f4e25f
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-12 22:45:04 +0200

    Add ProfileDTO

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/DTOs/ProfileDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustCommerce.Application.Common.DTOs
+{
+    public class ProfileDTO
+    {
+        public string FlagName { get; set; }
+        public int FlagValue { get; set; }
+    }
+}
