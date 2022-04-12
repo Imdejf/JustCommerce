db09aea6485eaa5ca85fb3a6b388a4c4c3f57e28
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-12 22:47:57 +0200

    Add JustCommerceJwtClaims model

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Models/JustCommerceJwtClaims.cs
@@ -0,0 +1,30 @@
+﻿using JustCommerce.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustCommerce.Application.Models
+{
+    public class JustCommerceJwtClaims : JwtClaims
+    {
+        public static new JustCommerceJwtClaims CreateFromJwtClaimsCollection(IEnumerable<Claim> claims)
+        {
+            var mockedJwtClaims = new JustCommerceJwtClaims();
+            return new JustCommerceJwtClaims
+            {
+                Email = claims.FirstOrDefault(c => c.Type == nameof(mockedJwtClaims.Email)).Value,
+                FirstName = claims.FirstOrDefault(c => c.Type == nameof(mockedJwtClaims.FirstName)).Value,
+                LastName = claims.FirstOrDefault(c => c.Type == nameof(mockedJwtClaims.LastName)).Value,
+                UserName = claims.FirstOrDefault(c => c.Type == nameof(mockedJwtClaims.UserName)).Value,
+                UserId = new Guid(claims.FirstOrDefault(c => c.Type == nameof(mockedJwtClaims.UserId)).Value),
+                PermissionsList = claims
+                    .Where(c => c.Type.EndsWith("LIST**"))
+                    .GroupBy(c => c.Type)
+                    .ToDictionary(c => c.Key, c => c.Select(c => int.Parse(c.Value)))
+            };
+        }
+    }
+}
