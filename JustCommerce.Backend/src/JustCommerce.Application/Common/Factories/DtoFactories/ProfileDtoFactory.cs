17483ee1a1a2a3a22ffb8caa095d6509a45675b4
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-12 22:45:38 +0200

    Add ProfileDTO

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Factories/DtoFactories/ProfileDtoFactory.cs
@@ -0,0 +1,16 @@
+﻿using JustCommerce.Application.Common.DTOs;
+
+namespace JustCommerce.Application.Common.Factories.DtoFactories
+{
+    public static class ProfileDtoFactory
+    {
+        public static ProfileDTO CreateFromData(string flagName, int flagValue)
+        {
+            return new ProfileDTO
+            {
+                FlagName = flagName,
+                FlagValue = flagValue
+            };
+        }
+    }
+}
