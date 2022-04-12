19c70bbc2bf9846708e9fdafcbdf406d743b54a2
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-12 22:44:33 +0200

    IJwtGenerator

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Interfaces/Service/IJwtGenerator.cs
@@ -0,0 +1,10 @@
+﻿using JustCommerce.Application.Common.DTOs;
+using JustCommerce.Application.Models;
+
+namespace JustCommerce.Application.Common.Interfaces.DataAccess.Service
+{
+    public interface IJwtGenerator
+    {
+        JwtGenerationResult Generate(UserDTO user);
+    }
+}
