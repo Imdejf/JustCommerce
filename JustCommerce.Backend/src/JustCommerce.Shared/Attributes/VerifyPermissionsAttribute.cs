ea78c303a3ae676824f3d00cad613bf17914c6f4
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-12 22:50:07 +0200

    Add attribute VerifyPermissionsAttribute

---
+++ JustCommerce.Backend/src/JustCommerce.Shared/Attributes/VerifyPermissionsAttribute.cs
@@ -0,0 +1,43 @@
+﻿using E_Commerce.Shared.Enums;
+using E_Commerce.Shared.Enums.Permissions;
+using E_Commerce.Shared.Services.Implementations.PermissionMapper;
+using E_Commerce.Shared.Services.Interfaces.Permission;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace E_Commerce.Shared.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public sealed class VerifyPermissionsAttribute : Attribute, IAuthorizationFilter
+    {
+        public int _RequiredPermissions;
+        public PermissionValidationMethod _Method;
+        public Type _PermissionDomainEnumType;
+
+        public VerifyPermissionsAttribute(AuthServicePermissions requiredPermissions, PermissionValidationMethod method) : this(requiredPermissions.GetType(), (int)requiredPermissions, method) { }
+
+        private VerifyPermissionsAttribute(Type type, int requiredPermissions, PermissionValidationMethod method)
+        {
+            _RequiredPermissions = requiredPermissions;
+            _Method = method;
+            _PermissionDomainEnumType = type;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            try
+            {
+                var token = context.HttpContext.Request.Headers["Authorization"].ToString().Split(" ")[1];
+
+                IPermissionValidator validator = new PermissionValidator();
+                if (!validator.HasPermissions(_PermissionDomainEnumType, _RequiredPermissions, token, _Method))
+                {
+                    throw new UnauthorizedAccessException();
+                }
+            }
+            catch (Exception)
+            {
+                throw new UnauthorizedAccessException();
+            }
+        }
+    }
+}
