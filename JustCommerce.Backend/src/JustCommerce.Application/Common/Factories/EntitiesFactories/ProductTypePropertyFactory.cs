37b3a6c201e60dec299f26f10e2bccf4f970743f
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-14 09:21:25 +0200

    Add ProductTypePropertyFactory

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Factories/EntitiesFactories/ProductTypePropertyFactory.cs
@@ -0,0 +1,23 @@
+﻿using JustCommerce.Application.Features.AdministrationFeatures.ProductTypeProperty.Command;
+using JustCommerce.Domain.Entities.ProductType;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustCommerce.Application.Common.Factories.EntitiesFactories
+{
+    public static class ProductTypePropertyFactory
+    {
+        public static ProductTypePropertyEntity CreateFromProductTypePropertyCommand(CreateProductTypeProperty.Command command)
+        {
+            return new ProductTypePropertyEntity
+            {
+                ProductTypeId = command.ProductTypeId,
+                OrderValue = command.OrderValue,
+                PropertyType = command.PropertyType,
+            };
+        }
+    }
+}
