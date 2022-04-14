11d354970fe36a16f9dc1fcfbd491a096f9576af
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-14 09:21:40 +0200

    Add UserPermissionEntity

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Factories/EntitiesFactories/UserPermissionEntityFactory.cs
@@ -0,0 +1,27 @@
+﻿using JustCommerce.Application.Common.DTOs;
+using JustCommerce.Domain.Entities.Identity;
+
+namespace JustCommerce.Application.Common.Factories.EntitiesFactories
+{
+    public static class UserPermissionEntityFactory
+    {
+        public static UserPermissionEntity CreateFromDtoAndUserId(PermissionDTO dto, Guid userId)
+        {
+            return new UserPermissionEntity
+            {
+                PermissionDomainName = dto.PermissionDomainName,
+                PermissionFlagValue = dto.PermissionFlagValue,
+                UserId = userId
+            };
+        }
+        public static UserPermissionEntity CreateFromData(string domainName, int flagValue, Guid userId)
+        {
+            return new UserPermissionEntity
+            {
+                PermissionDomainName = domainName,
+                PermissionFlagValue = flagValue,
+                UserId = userId
+            };
+        }
+    }
+}
