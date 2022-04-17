b7d290c24dd448a54fda6b9b241c250ae7d1af98
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-17 14:16:55 +0200

    Add ProductTypePropertyLangDtoFactory

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Factories/DtoFactories/ProductTypePropertyLangDtoFactory.cs
@@ -0,0 +1,20 @@
+﻿using JustCommerce.Application.Common.DTOs;
+using JustCommerce.Domain.Entities.ProductType;
+
+namespace JustCommerce.Application.Common.Factories.DtoFactories
+{
+    public static class ProductTypePropertyLangDtoFactory
+    {
+        public static ProductTypePropertyLangDTO CreateFromEntity(ProductTypePropertyLangEntity entity)
+        {
+            return new ProductTypePropertyLangDTO
+            {
+                Name = entity.Name,
+                DefualtValue = entity.DefualtValue,
+                IsoCode = entity.IsoCode,
+                Value = entity.Value,
+                ProductTypePropertyId = entity.ProductTypePropertyId
+            };
+        }
+    }
+}
