3fa4bfdfd703f9a7ab4f5698cafc9a689317d8b8
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-17 14:16:50 +0200

    Add ProductTypePropertyDtoFactory

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Factories/DtoFactories/ProductTypePropertyDtoFactory.cs
@@ -0,0 +1,18 @@
+﻿using JustCommerce.Application.Common.DTOs;
+using JustCommerce.Domain.Entities.ProductType;
+
+namespace JustCommerce.Application.Common.Factories.DtoFactories
+{
+    public static class ProductTypePropertyDtoFactory
+    {
+        public static ProductTypePropertyDTO CreateFromEntity(ProductTypePropertyEntity entity)
+        {
+            return new ProductTypePropertyDTO
+            {
+                OrderValue = entity.OrderValue,
+                PropertyType = entity.PropertyType,
+                ProductTypePropertyLangs = entity.ProductTypePropertyLang?.Select(c => ProductTypePropertyLangDtoFactory.CreateFromEntity(c)).ToArray()
+            };
+        }
+    }
+}
