ddb6654bce72508215d2f9ce796594321f082ad1
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-17 14:16:37 +0200

    Add ProductTypePropertyDTO

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/DTOs/ProductTypePropertyDTO.cs
@@ -0,0 +1,11 @@
+﻿using JustCommerce.Domain.Enums;
+
+namespace JustCommerce.Application.Common.DTOs
+{
+    public class ProductTypePropertyDTO
+    {
+        public int OrderValue { get; set; }
+        public PropertyType PropertyType { get; set; }
+        public ICollection<ProductTypePropertyLangDTO>? ProductTypePropertyLangs { get; set; }
+    }
+}
