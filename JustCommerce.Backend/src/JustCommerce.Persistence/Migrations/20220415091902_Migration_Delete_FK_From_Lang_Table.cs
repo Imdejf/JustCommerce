60bc94adcdab83d512c0e9c3ca4bf71d9f42bba7
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-17 14:17:57 +0200

    Add new migration Migration_Delete_FK_From_Lang_Table

---
+++ JustCommerce.Backend/src/JustCommerce.Persistence/Migrations/20220415091902_Migration_Delete_FK_From_Lang_Table.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JustCommerce.Persistence.Migrations
+{
+    public partial class Migration_Delete_FK_From_Lang_Table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ProductTypePropertyLang",
+                table: "ProductTypePropertyLang");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "ProductTypePropertyLang",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ProductTypePropertyLang",
+                table: "ProductTypePropertyLang",
+                columns: new[] { "ProductTypePropertyId", "Name", "Value" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ProductTypePropertyLang",
+                table: "ProductTypePropertyLang");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "ProductTypePropertyLang",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ProductTypePropertyLang",
+                table: "ProductTypePropertyLang",
+                column: "ProductTypePropertyId");
+        }
+    }
+}
