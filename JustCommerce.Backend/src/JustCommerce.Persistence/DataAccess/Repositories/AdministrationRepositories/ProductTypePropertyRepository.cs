bf56119126ddbed694d10e9a0012ea9bd21d14d5
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-17 14:17:47 +0200

    Add ProductTypePropertyRepository

---
+++ JustCommerce.Backend/src/JustCommerce.Persistence/DataAccess/Repositories/AdministrationRepositories/ProductTypePropertyRepository.cs
@@ -0,0 +1,16 @@
+﻿using JustCommerce.Application.Common.Interfaces.DataAccess.Repository.AdministrationRepositories;
+using JustCommerce.Domain.Entities.ProductType;
+using Microsoft.EntityFrameworkCore;
+
+namespace JustCommerce.Persistence.DataAccess.Repositories.AdministrationRepositories
+{
+    internal sealed class ProductTypePropertyRepository : BaseRepository<ProductTypePropertyEntity>, IProductTypePropertyRepository
+    {
+        public ProductTypePropertyRepository(DbSet<ProductTypePropertyEntity> entity) : base(entity) { }
+
+        public async Task AddRangeProductTypePropertyAsync(List<ProductTypePropertyEntity> productTypePropertyList, CancellationToken cancellationToken = default)
+        {
+            await _entity.AddRangeAsync(productTypePropertyList, cancellationToken);
+        }
+    }
+}
