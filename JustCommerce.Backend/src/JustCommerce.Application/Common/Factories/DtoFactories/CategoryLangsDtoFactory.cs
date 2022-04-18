560b765cb068cebabe47d342a871faeb98923a7c
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-18 23:58:11 +0200

    Add CategoryLangsDtoFactory

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Factories/DtoFactories/CategoryLangsDtoFactory.cs
@@ -0,0 +1,21 @@
+﻿using JustCommerce.Application.Common.DTOs;
+using JustCommerce.Domain.Entities.Category;
+
+namespace JustCommerce.Application.Common.Factories.DtoFactories
+{
+    public static class CategoryLangsDtoFactory
+    {
+        public static CategoryLangsDTO CreateFromEntity(CategoryLangEntity entity)
+        {
+            return new CategoryLangsDTO
+            {
+                CategoryId = entity.CategoryId,
+                Content = entity.Content,
+                Description = entity.Description,
+                IsoCode = entity.IsoCode,
+                Keywords = entity.Keywords,
+                Name = entity.Name,
+            };
+        }
+    }
+}
