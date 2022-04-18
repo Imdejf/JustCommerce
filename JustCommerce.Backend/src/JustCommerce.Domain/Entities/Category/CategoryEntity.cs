1b8b78b85767bb6a2ac0914a37dc917a4d55aac5
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-18 23:38:36 +0200

    Add CategoryEntity

---
+++ JustCommerce.Backend/src/JustCommerce.Domain/Entities/Category/CategoryEntity.cs
@@ -0,0 +1,13 @@
+﻿using JustCommerce.Domain.Entities.Abstract;
+
+namespace JustCommerce.Domain.Entities.Category
+{
+    public sealed class CategoryEntity : AuditableEntity
+    {
+        public string Slug { get; set; }
+        public int OrderValue { get; set; }
+        public string IconPath { get; set; }
+        public ICollection<SubCategoryEntity> SubCategory { get; set; }
+        public ICollection<CategoryLangEntity> CategoryLang { get; set; }
+    }
+}
---
+++ JustCommerce.Backend/src/JustCommerce.Domain/Entities/Category/CategoryLangEntity.cs
@@ -0,0 +1,15 @@
+﻿using JustCommerce.Domain.Entities.Abstract;
+
+namespace JustCommerce.Domain.Entities.Category
+{
+    public sealed class CategoryLangEntity : AuditableEntity
+    {
+        public Guid CategoryId { get; set; }
+        public CategoryEntity Category { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Keywords { get; set; }
+        public string Content { get; set; }
+        public string IsoCode { get; set; }
+    }
+}
