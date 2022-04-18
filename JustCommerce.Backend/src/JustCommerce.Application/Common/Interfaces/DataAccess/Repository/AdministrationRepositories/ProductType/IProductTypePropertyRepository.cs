f6810ee5173b2a88a23bb314faf5d4f275ce79e7
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-18 23:37:37 +0200

    Add IProductTypePropertyRepository

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Interfaces/DataAccess/Repository/AdministrationRepositories/ProductType/IProductTypePropertyRepository.cs
@@ -0,0 +1,10 @@
+﻿using JustCommerce.Application.Common.DataAccess.Repository;
+using JustCommerce.Domain.Entities.ProductType;
+
+namespace JustCommerce.Application.Common.Interfaces.DataAccess.Repository.AdministrationRepositories.ProductType
+{
+    public interface IProductTypePropertyRepository : IBaseRepository<ProductTypePropertyEntity>
+    {
+        Task AddRangeProductTypePropertyAsync(List<ProductTypePropertyEntity> productTypePropertyList, CancellationToken cancellationToken = default);
+    }
+}
