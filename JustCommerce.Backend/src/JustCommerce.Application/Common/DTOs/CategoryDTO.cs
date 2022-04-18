679b005957750dee32280338ea59f01fdb8ac8ae
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-18 23:37:12 +0200

    Add CategoryDTO

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/DTOs/CategoryDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace JustCommerce.Application.Common.DTOs
+{
+    public class CategoryDTO
+    {
+        public Guid CategoryId { get; set; }
+        public string Slug { get; set; }
+        public int OrderValue { get; set; }
+        public string IconPath { get; set; }
+        public ICollection<CategoryLangsDTO>? CategoryLangs { get; set; }
+    }
+}
