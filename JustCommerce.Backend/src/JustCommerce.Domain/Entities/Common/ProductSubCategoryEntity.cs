95dcd47a31b89c785696ad16ac3833b6ed258027
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-19 16:22:58 +0200

    Add ProductSubCategoryEntity

---
+++ JustCommerce.Backend/src/JustCommerce.Domain/Entities/Common/ProductSubCategoryEntity.cs
@@ -0,0 +1,13 @@
+﻿using JustCommerce.Domain.Entities.Category;
+using JustCommerce.Domain.Entities.Product;
+
+namespace JustCommerce.Domain.Entities.Common
+{
+    public class ProductSubCategoryEntity
+    {
+        public Guid SubCategoryId { get; set; }
+        public SubCategoryEntity SubCategory { get; set; }
+        public Guid ProductId { get; set; }
+        public ProductEntity Product { get; set; }
+    }
+}
