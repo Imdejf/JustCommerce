5edbe646ae9c3489e6c4edf5dff26e173b2ff708
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-19 16:24:00 +0200

    Add ProductLangConfig

---
+++ JustCommerce.Backend/src/JustCommerce.Persistence/DataAccess/EntitiesConfig/Product/ProductLangConfig.cs
@@ -0,0 +1,68 @@
+﻿using JustCommerce.Domain.Entities.Product;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace JustCommerce.Persistence.DataAccess.EntitiesConfig.Product
+{
+    public sealed class ProductLangConfig : IEntityTypeConfiguration<ProductLangEntity>
+    {
+        public void Configure(EntityTypeBuilder<ProductLangEntity> builder)
+        {
+            builder.ToTable("ProductLang");
+
+            builder.HasKey(c => c.Id);
+
+            builder.HasOne(c => c.Product)
+                   .WithMany(c => c.ProductLang)
+                   .HasForeignKey(c => c.ProductId);
+
+            builder.Property(c => c.Name)
+                   .HasColumnType("varchar")
+                   .HasMaxLength(250)
+                   .IsRequired();
+
+            builder.Property(c => c.Description)
+                   .HasColumnType("varchar")
+                   .HasMaxLength(1000)
+                   .IsRequired();
+
+            builder.Property(c => c.MetaTitle)
+                   .HasColumnType("varchar")
+                   .HasMaxLength(250)
+                   .IsRequired();
+
+            builder.Property(c => c.MetaDescription)
+                   .HasColumnType("varchar")
+                   .HasMaxLength(500)
+                   .IsRequired();
+
+            builder.Property(c => c.Synonyms)
+                   .HasColumnType("varchar")
+                   .HasMaxLength(250)
+                   .IsRequired();
+
+            builder.Property(c => c.ImageDescription)
+                   .HasColumnType("varchar")
+                   .HasMaxLength(250)
+                   .IsRequired();
+
+            builder.Property(c => c.Tags)
+                   .HasColumnType("varchar")
+                   .HasMaxLength(250)
+                   .IsRequired();
+
+            builder.Property(c => c.Keywords)
+                   .HasColumnType("varchar")
+                   .HasMaxLength(250)
+                   .IsRequired();
+
+            builder.Ignore(c => c.CreatedDate);
+
+            builder.Ignore(c => c.CreatedBy);
+
+            builder.Ignore(c => c.LastModifiedBy);
+
+            builder.Ignore(c => c.LastModifiedDate);
+        }
+    }
+}
