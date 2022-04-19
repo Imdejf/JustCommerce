df308b065736a2efdd681470233c33e9cc51c97c
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-19 16:22:51 +0200

    Add ProductCategoryEntity

---
+++ JustCommerce.Backend/src/JustCommerce.Domain/Entities/Common/ProductCategoryEntity.cs
@@ -0,0 +1,13 @@
+﻿using JustCommerce.Domain.Entities.Category;
+using JustCommerce.Domain.Entities.Product;
+
+namespace JustCommerce.Domain.Entities.Common
+{
+    public class ProductCategoryEntity
+    {
+        public Guid CategoryId { get; set; }
+        public CategoryEntity Category { get; set; }
+        public Guid ProductId { get; set; }
+        public ProductEntity Product { get; set; }
+    }
+}
