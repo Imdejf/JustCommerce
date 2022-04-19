be5d527cc7975ef5b301c7d815bd0686e0a5a7b0
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-19 16:23:30 +0200

    Add ProductSubCategoryConfig

---
+++ JustCommerce.Backend/src/JustCommerce.Persistence/DataAccess/EntitiesConfig/Common/ProductSubCategoryConfig.cs
@@ -0,0 +1,26 @@
+﻿using JustCommerce.Domain.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace JustCommerce.Persistence.DataAccess.EntitiesConfig.Common
+{
+    public sealed class ProductSubCategoryConfig : IEntityTypeConfiguration<ProductSubCategoryEntity>
+    {
+        public void Configure(EntityTypeBuilder<ProductSubCategoryEntity> builder)
+        {
+            builder.ToTable("ProductSubCategory","common");
+
+            builder.HasKey(c => new { c.ProductId, c.SubCategoryId });
+
+            builder.HasOne(pt => pt.Product)
+                .WithMany(t => t.ProductSubCategory)
+                .HasForeignKey(pt => pt.ProductId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(pt => pt.SubCategory)
+                .WithMany(t => t.ProductSubCategory)
+                .HasForeignKey(am => am.SubCategoryId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
