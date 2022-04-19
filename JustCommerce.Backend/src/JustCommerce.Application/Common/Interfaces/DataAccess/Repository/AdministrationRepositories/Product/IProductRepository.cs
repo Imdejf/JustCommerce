43f6611f714b4913da32025be04ce2a26f1ee2c3
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-19 16:21:57 +0200

    Add IProductRepository

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Interfaces/DataAccess/Repository/AdministrationRepositories/Product/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using JustCommerce.Application.Common.DataAccess.Repository;
+using JustCommerce.Domain.Entities.Product;
+
+namespace JustCommerce.Application.Common.Interfaces.DataAccess.Repository.AdministrationRepositories.Product
+{
+    public interface IProductRepository : IBaseRepository<ProductEntity>
+    {
+        Task<bool> ExistSlugAsync(string slug, CancellationToken cancellationToken = default);
+        Task<ProductEntity> GetCategoryById(Guid categoryId, CancellationToken cancellationToken = default);
+    }
+}
