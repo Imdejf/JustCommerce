21b1deb5925649d9b757e3dd395c59f2fa20e3fa
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-19 16:20:51 +0200

    ProductLangDTO

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/DTOs/Product/ProductLangDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace JustCommerce.Application.Common.DTOs.Product
+{
+    public class ProductLangDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string MetaTitle { get; set; }
+        public string MetaDescription { get; set; }
+        public string Synonyms { get; set; }
+        public string Tags { get; set; }
+        public string Keywords { get; set; }
+        public string ImageDescription { get; set; }
+        public string IsoCode { get; set; }
+    }
+}
