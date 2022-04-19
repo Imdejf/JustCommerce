c7a54b29d9e0c61c2475a06a11e9163076606769
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-19 16:23:04 +0200

    Add ProductEntity

---
+++ JustCommerce.Backend/src/JustCommerce.Domain/Entities/Product/ProductEntity.cs
@@ -0,0 +1,22 @@
+﻿using JustCommerce.Domain.Entities.Abstract;
+using JustCommerce.Domain.Entities.Category;
+using JustCommerce.Domain.Entities.Common;
+using JustCommerce.Domain.Entities.ProductType;
+
+namespace JustCommerce.Domain.Entities.Product
+{
+    public class ProductEntity : AuditableEntity
+    {
+        public Guid ProductTypeId { get; set; }
+        public ProductTypeEntity ProductType { get; set; }
+        public string Slug { get; set; }
+        public bool Top { get; set; }
+        public bool AvailabilityType { get; set; } //
+        public bool Active { get; set; }
+        public bool Newsletter { get; set; }
+
+        public ICollection<ProductCategoryEntity> ProductCategory { get; set; }
+        public ICollection<ProductSubCategoryEntity> ProductSubCategory { get; set; }
+        public ICollection<ProductLangEntity> ProductLang { get; set; }
+    }
+}
