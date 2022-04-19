87a53dfc74aa04cd8f22d3b809c92057a1688506
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-19 16:23:25 +0200

    Add ProductCategoryConfig

---
+++ JustCommerce.Backend/src/JustCommerce.Persistence/DataAccess/EntitiesConfig/Common/ProductCategoryConfig.cs
@@ -0,0 +1,31 @@
+﻿using JustCommerce.Domain.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustCommerce.Persistence.DataAccess.EntitiesConfig.Common
+{
+    public class ProductCategoryConfig : IEntityTypeConfiguration<ProductCategoryEntity>
+    {
+        public void Configure(EntityTypeBuilder<ProductCategoryEntity> builder)
+        {
+            builder.ToTable("ProductCategory", "common");
+
+            builder.HasKey(c => new { c.ProductId, c.CategoryId });
+
+            builder.HasOne(c => c.Product)
+                .WithMany(c => c.ProductCategory)
+                .HasForeignKey(c => c.ProductId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(c => c.Category)
+                .WithMany(c => c.ProductCategory)
+                .HasForeignKey(c => c.CategoryId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
