2bba14d9cc268768b669b573941261c74ff0cd46
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-19 16:21:52 +0200

    Add ISubCategoryRepository

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Factories/EntitiesFactories/Product/ProductEntityFactory.cs
@@ -0,0 +1,21 @@
+﻿using JustCommerce.Application.Features.AdministrationFeatures.Product.Command;
+using JustCommerce.Domain.Entities.Product;
+
+namespace JustCommerce.Application.Common.Factories.EntitiesFactories.Product
+{
+    public static class ProductEntityFactory
+    {
+        public static ProductEntity CreateFromCategoryCommand(CreateProduct.Command command)
+        {
+            return new ProductEntity
+            {
+                ProductTypeId = command.ProductTypeId,
+                Active = command.Active,
+                AvailabilityType = command.AvailabilityType,
+                Newsletter = command.Newsletter,
+                Slug = command.Slug,
+                Top = command.Top,
+            };
+        }
+    }
+}
---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Interfaces/DataAccess/Repository/AdministrationRepositories/Category/ISubCategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using JustCommerce.Application.Common.DataAccess.Repository;
+using JustCommerce.Domain.Entities.Category;
+
+namespace JustCommerce.Application.Common.Interfaces.DataAccess.Repository.AdministrationRepositories.Category
+{
+    public interface ISubCategoryRepository : IBaseRepository<SubCategoryEntity>
+    {
+        Task<bool> ExistSlugAsync(string slug, CancellationToken cancellationToken = default);
+        Task<SubCategoryEntity> GetSubCategoryById(Guid subCategoryId, CancellationToken cancellationToken = default);
+    }
+}
