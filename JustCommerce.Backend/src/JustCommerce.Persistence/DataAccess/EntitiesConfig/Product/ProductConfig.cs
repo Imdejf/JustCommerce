56820b78f07e47174c95abd1d53a3efbfc35fa3d
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-19 16:23:54 +0200

    Add ProductConfig

---
+++ JustCommerce.Backend/src/JustCommerce.Persistence/DataAccess/EntitiesConfig/Product/ProductConfig.cs
@@ -0,0 +1,67 @@
+﻿using JustCommerce.Domain.Entities.Product;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustCommerce.Persistence.DataAccess.EntitiesConfig.Product
+{
+    public sealed class ProductConfig : IEntityTypeConfiguration<ProductEntity>
+    {
+        public void Configure(EntityTypeBuilder<ProductEntity> builder)
+        {
+            builder.ToTable("Product");
+
+            builder.HasKey(c => c.Id);
+            builder.HasIndex(c => c.Id);
+
+            builder.Property(c => c.Id)
+                   .ValueGeneratedOnAdd();
+
+            builder.HasOne(c => c.ProductType)
+                   .WithMany(c => c.Product)
+                   .HasForeignKey(c => c.ProductTypeId);
+
+            builder.HasMany(c => c.ProductLang)
+                   .WithOne(c => c.Product)
+                   .HasForeignKey(c => c.ProductId);
+
+            builder.Property(c => c.AvailabilityType)
+                   .HasColumnType("bit");
+
+            builder.Property(c => c.Newsletter)
+                   .HasColumnType("bit");
+
+            builder.Property(c => c.Active)
+                   .HasColumnType("bit");
+
+            builder.Property(c => c.Slug)
+                   .HasColumnType("varchar")
+                   .HasMaxLength(500);
+
+            builder.Property(c => c.Top)
+                   .HasColumnType("bit");
+
+            builder.Property(c => c.CreatedBy)
+                   .HasColumnType("varchar")
+                   .HasMaxLength(50)
+                   .IsRequired(false);
+
+            builder.Property(c => c.CreatedDate)
+                   .HasColumnType("datetime")
+                   .IsRequired();
+
+            builder.Property(c => c.LastModifiedBy)
+                    .HasColumnType("varchar")
+                    .HasMaxLength(50)
+                    .IsRequired(false);
+
+            builder.Property(c => c.LastModifiedDate)
+                   .HasColumnType("datetime")
+                   .IsRequired(false);
+        }
+    }
+}
