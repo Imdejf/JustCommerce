0aa577e41e595a378f5d92a77aeb3aaac8360585
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-19 16:24:26 +0200

    Add ProductRepository

---
+++ JustCommerce.Backend/src/JustCommerce.Persistence/DataAccess/Repositories/AdministrationRepositories/Product/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using JustCommerce.Application.Common.Interfaces.DataAccess.Repository.AdministrationRepositories.Product;
+using JustCommerce.Domain.Entities.Product;
+using Microsoft.EntityFrameworkCore;
+
+namespace JustCommerce.Persistence.DataAccess.Repositories.AdministrationRepositories.Product
+{
+    internal sealed class ProductRepository : BaseRepository<ProductEntity>, IProductRepository
+    {
+        public ProductRepository(DbSet<ProductEntity> entity) : base(entity)
+        {
+        }
+
+        public async Task<bool> ExistSlugAsync(string slug, CancellationToken cancellationToken = default)
+        {
+            return await _entity.Where(c => c.Slug == slug).AnyAsync(cancellationToken);
+        }
+
+        public async Task<ProductEntity> GetCategoryById(Guid productId, CancellationToken cancellationToken = default)
+        {
+            return await _entity
+                        .Include(c => c.ProductType)
+                        .Include(c => c.ProductLang)
+                        .FirstOrDefaultAsync(c => c.Id == productId, cancellationToken);
+        }
+    }
+}
