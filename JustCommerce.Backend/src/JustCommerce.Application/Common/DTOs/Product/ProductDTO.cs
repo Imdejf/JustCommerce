8f1a0408f1d85344fc473c3b1099fd81327543e4
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-19 16:20:27 +0200

    Add ProductDTO

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/DTOs/Product/ProductDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace JustCommerce.Application.Common.DTOs.Product
+{
+    public class ProductDTO
+    {
+        public string Slug { get; set; }
+        public bool Top { get; set; }
+        public bool AvailabilityType { get; set; }
+        public bool Active { get; set; }
+        public bool Newsletter { get; set; }
+
+        public ICollection<ProductLangDTO> ProductLang { get; set; }
+    }
+}
