e5330a373e22fcaca39ab07d0b92110d5bb2b9d8
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-21 21:13:57 +0200

    Add ICurrentUserServiceTests

---
+++ JustCommerce.Backend/tests/Shared/Shared.UnitTests/ServicesTests/ICurrentUserServiceTests.cs
@@ -0,0 +1,61 @@
+﻿using JustCommerce.Shared.Models;
+using JustCommerce.Shared.Services.Interfaces;
+using Xunit;
+
+namespace Shared.UnitTests.ServicesTests
+{
+    public sealed class ICurrentUserServiceTests : IClassFixture<ServiceCollection>
+    {
+        private readonly ServiceCollection _Services;
+        private readonly ICurrentUserService _CurrentUserService;
+        public ICurrentUserServiceTests(ServiceCollection services)
+        {
+            _Services = services;
+            _CurrentUserService = _Services.GetService<ICurrentUserService>();
+        }
+
+        [Fact]
+        public void CurrentUser_Is_Not_Null_After_Initialize()
+        {
+            Assert.NotNull(_CurrentUserService.CurrentUser);
+        }
+
+        [Fact]
+        public void CurrentUser_Changes_After_SetCurrentUserCall_With_Claims()
+        {
+            var prevUser = _CurrentUserService.CurrentUser;
+
+            _CurrentUserService.SetCurrentUser(new JwtClaims
+            {
+                Email = "NEWTESTMAIL@gmail.com",
+                Id = System.Guid.NewGuid()
+            });
+
+            Assert.NotEqual(prevUser.Email, _CurrentUserService.CurrentUser.Email);
+            Assert.NotEqual(prevUser.Id, _CurrentUserService.CurrentUser.Id);
+        }
+
+        [Fact]
+        public void CurrentUser_Dont_Changes_After_SetCurrentUserCall_With_Invalid_Jwt()
+        {
+            var prevUser = _CurrentUserService.CurrentUser;
+
+            _CurrentUserService.SetCurrentUser(_Services.InvalidJwt);
+
+            Assert.Equal(prevUser.Email, _CurrentUserService.CurrentUser.Email);
+            Assert.Equal(prevUser.Id, _CurrentUserService.CurrentUser.Id);
+        }
+
+        [Fact]
+
+        public void CurrentUser_Changes_After_SetCurrentUserCall_With_Valid_Jwt()
+        {
+            var prevUser = _CurrentUserService.CurrentUser;
+
+            _CurrentUserService.SetCurrentUser(_Services.ValidJwt);
+
+            Assert.NotEqual(prevUser.Email, _CurrentUserService.CurrentUser.Email);
+            Assert.NotEqual(prevUser.Id, _CurrentUserService.CurrentUser.Id);
+        }
+    }
+}
