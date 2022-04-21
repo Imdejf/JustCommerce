979c3d0b0e8106dbf9dc52314ea09b669a51c455
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-21 21:15:18 +0200

    Add AddFluentValidatorsTests

---
+++ JustCommerce.Backend/tests/Shared/Shared.UnitTests/DependencyInjectionTests/AddFluentValidatorsTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Shared.UnitTests.DependencyInjectionTests
+{
+    public sealed class AddFluentValidatorsTests
+    {
+        [Fact]
+        public void FluentValidation_Is_Registered_Properly()
+        {
+            IServiceCollection services = new Microsoft.Extensions.DependencyInjection.ServiceCollection();
+            services.AddFluentValidation();
+            var serviceProvider = services.BuildServiceProvider();
+
+            Assert.NotNull(serviceProvider.GetRequiredService<IValidatorFactory>());
+        }
+    }
+}
