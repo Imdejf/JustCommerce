3fb19b2d379b75f82c2c19fcce972cbebdf4a214
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-23 16:58:08 +0200

    Add ProductFileEntity

---
+++ JustCommerce.Backend/src/JustCommerce.Domain/Entities/Product/ProductFileEntity.cs
@@ -0,0 +1,12 @@
+﻿using JustCommerce.Domain.Enums;
+
+namespace JustCommerce.Domain.Entities.Product
+{
+    public class ProductFileEntity
+    {
+        public Guid ProductId { get; set; }
+        public ProductEntity Product { get; set; }
+        public ProductColor ProductColor { get; set; }
+        public string PhotoPath { get; set; }
+    }
+}
