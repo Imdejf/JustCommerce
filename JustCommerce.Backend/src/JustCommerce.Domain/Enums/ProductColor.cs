ff223135ef29921d72ea5382f43b1049aa7dd84c
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-23 16:58:15 +0200

    Add ProductColor

---
+++ JustCommerce.Backend/src/JustCommerce.Domain/Enums/ProductColor.cs
@@ -0,0 +1,15 @@
+﻿namespace JustCommerce.Domain.Enums
+{
+    public enum ProductColor
+    {
+        Defualt = 0,
+        White = 1,
+        Red = 2,
+        Orange = 3,
+        Yellow = 4,
+        Green = 5,
+        Blue = 6,
+        Grey = 7,
+        Brown = 8,
+    }
+}
