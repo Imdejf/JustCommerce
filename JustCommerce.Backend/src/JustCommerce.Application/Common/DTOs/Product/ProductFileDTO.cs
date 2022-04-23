01e4ce7b9a48f638a361ef12334ca0f7c14b6dfe
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-23 16:57:59 +0200

    Add ProductFileDTO

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/DTOs/Product/ProductFileDTO.cs
@@ -0,0 +1,11 @@
+﻿using JustCommerce.Domain.Enums;
+using JustCommerce.Shared.Models;
+
+namespace JustCommerce.Application.Common.DTOs.Product
+{
+    public class ProductFileDTO
+    {
+        public Base64File Base64File { get; set; }
+        public ProductColor ProductColor { get; set; }
+    }
+}
