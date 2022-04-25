00b35325c4498936a5352775e7b75f2b6954455d
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-25 23:25:31 +0200

    Add implementation DeleteShop

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Features/ManagemenetFeatures/Shop/Command/DeleteShop.cs
@@ -0,0 +1,53 @@
+﻿using FluentValidation;
+using JustCommerce.Application.Common.DataAccess.Repository;
+using JustCommerce.Application.Common.Interfaces;
+using JustCommerce.Shared.Exceptions;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JustCommerce.Application.Features.ManagemenetFeatures.Shop.Command
+{
+    public static class DeleteShop
+    {
+
+        public sealed record Command(Guid ShopId) : IRequestWrapper<Unit>;
+
+        public sealed class Handler : IRequestHandlerWrapper<Command, Unit>
+        {
+            private readonly IUnitOfWorkManagmenet _unitOfWorkManagmenet;
+            public Handler(IUnitOfWorkManagmenet unitOfWorkManagmenet)
+            {
+                _unitOfWorkManagmenet = unitOfWorkManagmenet;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var shopExist = await _unitOfWorkManagmenet.Shop.GetByIdAsync(request.ShopId, cancellationToken);
+
+                if (shopExist is null)
+                {
+                    throw new EntityNotFoundException($"Shop with Id : {request.ShopId} doesn`t exists");
+                }
+
+                _unitOfWorkManagmenet.Shop.Remove(shopExist);
+                await _unitOfWorkManagmenet.SaveChangesAsync(cancellationToken);
+
+                return Unit.Value;
+            }
+        }
+
+        public sealed class Validator : AbstractValidator<Command>
+        {
+            public Validator()
+            {
+                RuleFor(c => c.ShopId).NotEqual(Guid.Empty);
+            }
+        }
+
+    }
+}
