065653957f616537598333508daddf9e136f8506
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-25 23:24:42 +0200

    Add new migration Migration_Create_Product_File_Table

--- JustCommerce.Backend/src/JustCommerce.Persistence/Migrations/20220425204207_Migration_Create_Product_File_Table.Designer.cs
+++ JustCommerce.Backend/src/JustCommerce.Persistence/Migrations/20220425204207_Migration_Create_Product_File_Table.Designer.cs
@@ -4,6 +4,7 @@ using JustCommerce.Persistence.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace JustCommerce.Persistence.Migrations
 {
     [DbContext(typeof(JustCommerceDbContext))]
-    partial class JustCommerceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220425204207_Migration_Create_Product_File_Table")]
+    partial class Migration_Create_Product_File_Table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
