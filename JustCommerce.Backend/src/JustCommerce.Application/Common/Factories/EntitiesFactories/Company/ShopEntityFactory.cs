ee2d5ff721e169c5b2e5d67224418c9133d47175
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-25 23:26:24 +0200

    Add ShopEntityFactory

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Factories/EntitiesFactories/Company/ShopEntityFactory.cs
@@ -0,0 +1,25 @@
+﻿using JustCommerce.Application.Features.ManagemenetFeatures.Shop.Command;
+using JustCommerce.Domain.Entities.Company;
+
+namespace JustCommerce.Application.Common.Factories.EntitiesFactories.Company
+{
+    public static class ShopEntityFactory
+    {
+        public static ShopEntity CreateFromShopCommand(CreateShop.Command command)
+        {
+            return new ShopEntity
+            {
+                Id = command.Id,
+                State = command.State,
+                Active = command.Active,
+                AddressLine = command.AddressLine,
+                City = command.City,
+                Country = command.Country,
+                Email = command.Email,
+                FullName = command.FullName,
+                Name = command.Name,
+                Zip = command.Zip
+            };
+        }
+    }
+}
