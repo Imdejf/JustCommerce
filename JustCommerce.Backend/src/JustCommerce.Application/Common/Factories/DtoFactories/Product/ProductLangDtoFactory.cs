463ebd337d8037378f77fa7d1bc0428ed5c6f6d1
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-25 23:26:42 +0200

    Add ProductLangDtoFactory

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Factories/DtoFactories/Product/ProductLangDtoFactory.cs
@@ -0,0 +1,24 @@
+﻿using JustCommerce.Application.Common.DTOs.Product;
+using JustCommerce.Domain.Entities.Product;
+
+namespace JustCommerce.Application.Common.Factories.DtoFactories.Product
+{
+    public static class ProductLangDtoFactory
+    {
+        public static ProductLangDTO CreateFromEntity(ProductLangEntity product)
+        {
+            return new ProductLangDTO
+            {
+                Name = product.Name,
+                Description = product.Description,
+                Synonyms = product.Synonyms,
+                ImageDescription = product.ImageDescription,
+                Keywords = product.Keywords,
+                MetaDescription = product.MetaDescription,
+                MetaTitle = product.MetaTitle,
+                Tags = product.Tags,
+                IsoCode = product.IsoCode,
+            };
+        }
+    }
+}
