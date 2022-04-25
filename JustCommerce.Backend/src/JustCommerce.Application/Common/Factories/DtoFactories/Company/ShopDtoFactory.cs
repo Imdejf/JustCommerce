5f47378c21e51a68d85f434ddd433697f8a0f9f9
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-25 23:26:32 +0200

    Add ShopDtoFactory

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Factories/DtoFactories/Company/ShopDtoFactory.cs
@@ -0,0 +1,25 @@
+﻿using JustCommerce.Application.Common.DTOs.Company;
+using JustCommerce.Domain.Entities.Company;
+
+namespace JustCommerce.Application.Common.Factories.DtoFactories.Company
+{
+    public static class ShopDtoFactory
+    {
+        public static ShopDTO CreateFromEntity(ShopEntity shop)
+        {
+            return new ShopDTO
+            {
+                Id = shop.Id,
+                State = shop.State,
+                City = shop.City,
+                Active = shop.Active,
+                AddressLine = shop.AddressLine,
+                Country = shop.Country,
+                Email = shop.Email,
+                FullName = shop.FullName,
+                Name = shop.Name,
+                Zip = shop.Zip
+            };
+        }
+    }
+}
