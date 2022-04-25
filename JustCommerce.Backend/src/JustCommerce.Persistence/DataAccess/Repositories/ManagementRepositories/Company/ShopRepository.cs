4938b3dc588c25e0f7ad2a4114512ca129823dc3
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-25 23:24:50 +0200

    Add ShopRepository

---
+++ JustCommerce.Backend/src/JustCommerce.Persistence/DataAccess/Repositories/ManagementRepositories/Company/ShopRepository.cs
@@ -0,0 +1,13 @@
+﻿using JustCommerce.Application.Common.Interfaces.DataAccess.Repository.ManagementRepositories.Company;
+using JustCommerce.Domain.Entities.Company;
+using Microsoft.EntityFrameworkCore;
+
+namespace JustCommerce.Persistence.DataAccess.Repositories.ManagementRepositories.Company
+{
+    internal class ShopRepository : BaseRepository<ShopEntity>, IShopRepository
+    {
+        public ShopRepository(DbSet<ShopEntity> entity) : base(entity)
+        {
+        }
+    }
+}
