fa4519e0b78eb662648754d931ba4ca7f3d9d4ae
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-25 23:25:00 +0200

    Add ProductFileConfig

---
+++ JustCommerce.Backend/src/JustCommerce.Persistence/DataAccess/EntitiesConfig/Product/ProductFileConfig.cs
@@ -0,0 +1,47 @@
+﻿using JustCommerce.Domain.Entities.Product;
+using JustCommerce.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace JustCommerce.Persistence.DataAccess.EntitiesConfig.Product
+{
+    public sealed class ProductFileConfig : IEntityTypeConfiguration<ProductFileEntity>
+    {
+        public void Configure(EntityTypeBuilder<ProductFileEntity> builder)
+        {
+            builder.ToTable("ProductFile");
+            
+            builder.HasKey(c => new { c.ProductId, c.PhotoPath });
+
+            builder.HasIndex(c => c.Id);
+
+            builder.Property(c => c.Id).ValueGeneratedOnAdd();
+
+            builder.HasOne(c => c.Product)
+                   .WithMany(c => c.ProductFile)
+                   .HasForeignKey(c => c.ProductId)
+                   .OnDelete(DeleteBehavior.Cascade)
+                   .IsRequired(true);
+
+            builder.Property(c => c.PhotoPath)
+                   .HasColumnType("varchar(250)")
+                   .IsRequired();
+
+            builder.Property(c => c.ProductColor)
+                   .HasColumnType("varchar")
+                   .HasMaxLength(50)
+                   .IsRequired()
+                   .HasConversion(
+                   x => x.ToString(),
+                   x => (ProductColor)Enum.Parse(typeof(ProductColor), x, true));
+
+            builder.Ignore(c => c.CreatedDate);
+
+            builder.Ignore(c => c.CreatedBy);
+
+            builder.Ignore(c => c.LastModifiedBy);
+
+            builder.Ignore(c => c.LastModifiedDate);
+        }
+    }
+}
