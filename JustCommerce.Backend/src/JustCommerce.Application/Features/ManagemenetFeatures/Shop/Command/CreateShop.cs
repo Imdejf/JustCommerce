c0713ad73afe2ce02fef8c0232482198269f2a55
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-25 23:25:16 +0200

    Add implementation CreateShop

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Features/ManagemenetFeatures/Shop/Command/CreateShop.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation;
+using JustCommerce.Application.Common.DataAccess.Repository;
+using JustCommerce.Application.Common.DTOs.Company;
+using JustCommerce.Application.Common.Factories.DtoFactories.Company;
+using JustCommerce.Application.Common.Factories.EntitiesFactories.Company;
+using JustCommerce.Application.Common.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JustCommerce.Application.Features.ManagemenetFeatures.Shop.Command
+{
+    public static class CreateShop
+    {
+
+        public sealed record Command(Guid Id, string Name, string State,  bool Active, string AddressLine, string City, string Country, string Email, string FullName, string Zip) : IRequestWrapper<ShopDTO>;
+
+        public sealed class Handler : IRequestHandlerWrapper<Command, ShopDTO>
+        {
+            private readonly IUnitOfWorkManagmenet _unitOfWorkManagmenet;
+            public Handler(IUnitOfWorkManagmenet unitOfWorkManagmenet)
+            {
+                _unitOfWorkManagmenet = unitOfWorkManagmenet;
+            }
+
+            public async Task<ShopDTO> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var newShop = ShopEntityFactory.CreateFromShopCommand(request);
+                await _unitOfWorkManagmenet.Shop.AddAsync(newShop, cancellationToken);
+
+                return ShopDtoFactory.CreateFromEntity(newShop);
+            }
+        }
+
+        public sealed class Validator : AbstractValidator<Command>
+        {
+            public Validator()
+            {
+
+            }
+        }
+
+    }
+}
