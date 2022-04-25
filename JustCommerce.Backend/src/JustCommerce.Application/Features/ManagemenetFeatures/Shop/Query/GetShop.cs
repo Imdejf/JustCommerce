84aca16eb2e7b3c73dd64321d913ada14b04af8f
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-25 23:25:56 +0200

    Add implementation GetShop

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Features/ManagemenetFeatures/Shop/Query/GetShop.cs
@@ -0,0 +1,47 @@
+﻿using FluentValidation;
+using JustCommerce.Application.Common.DataAccess.Repository;
+using JustCommerce.Application.Common.DTOs.Company;
+using JustCommerce.Application.Common.Factories.DtoFactories.Company;
+using JustCommerce.Application.Common.Interfaces;
+using JustCommerce.Shared.Exceptions;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JustCommerce.Application.Features.ManagemenetFeatures.Shop.Query
+{
+    public static class GetShop
+    {
+
+        public sealed record Query() : IRequestWrapper<List<ShopDTO>>;
+
+        public sealed class Handler : IRequestHandlerWrapper<Query, List<ShopDTO>>
+        {
+            private readonly IUnitOfWorkManagmenet _unitOfWorkManagmenet;
+            public Handler(IUnitOfWorkManagmenet unitOfWorkManagmenet)
+            {
+                _unitOfWorkManagmenet = unitOfWorkManagmenet;
+            }
+
+            public async Task<List<ShopDTO>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var shopList = await _unitOfWorkManagmenet.Shop.GetAllAsync(cancellationToken);
+
+                return shopList.Select(c => ShopDtoFactory.CreateFromEntity(c)).ToList();
+            }
+        }
+
+        public sealed class Validator : AbstractValidator<Query>
+        {
+            public Validator()
+            {
+
+            }
+        }
+
+    }
+}
