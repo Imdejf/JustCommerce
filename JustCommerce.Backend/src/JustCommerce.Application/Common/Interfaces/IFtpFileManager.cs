8827772e1e9ae3dc488a8b0e9cb9ca51d788d1c5
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-25 23:52:50 +0200

    Add FtpFileManager

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Interfaces/IFtpFileManager.cs
@@ -0,0 +1,10 @@
+﻿using JustCommerce.Shared.Models;
+
+namespace JustCommerce.Application.Common.Interfaces
+{
+    public interface IFtpFileManager
+    {
+        Task<string> SavePostPhotoOnFtpAsync(Base64File file, CancellationToken cancellationToken = default);
+        Task RemoveFileFromFtpAsync(string ftpFilePath, CancellationToken cancellationToken = default);
+    }
+}
---
+++ JustCommerce.Backend/src/JustCommerce.Infrastructure/Implementations/FtpFileManager.cs
@@ -0,0 +1,32 @@
+﻿using JustCommerce.Shared.Exceptions;
+using JustCommerce.Shared.Models;
+
+namespace JustCommerce.Infrastructure.Implementations
+{
+    internal sealed class FtpFileManager : IFtpFileManager
+    {
+        private readonly DataSharp.FtpFileManagement.Interfaces.IFtpFileManager _FtpFileManager;
+
+        public FtpFileManager(DataSharp.FtpFileManagement.Interfaces.IFtpFileManager ftpFileManager)
+        {
+            _FtpFileManager = ftpFileManager;
+        }
+        public async Task RemoveFileFromFtpAsync(string ftpFilePath, CancellationToken cancellationToken = default)
+        {
+            if (!await _FtpFileManager.ExistsAsync(ftpFilePath))
+            {
+                throw new EntityNotFoundException($"File with {ftpFilePath} path not exist", 0);
+            }
+            await _FtpFileManager.RemoveAsync(ftpFilePath);
+        }
+
+        public async Task<string> SavePostPhotoOnFtpAsync(Base64File file, CancellationToken cancellationToken = default)
+        {
+            var filePath = Guid.NewGuid().ToString();
+            var connection = _FtpFileManager.GetCurrentConnection();
+            var ftpFilePath = @$"{connection.Value.HttpUri}{connection.Value.RootFolder}/{filePath}{file.FileExtension}";
+            await _FtpFileManager.CreateAsync(ftpFilePath, file.ByteArray);
+            return ftpFilePath;
+        }
+    }
+}
