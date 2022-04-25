29c0466e9960352f83ff18fb98e8aea27fad06a7
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-25 23:26:54 +0200

    Add IShopRepository

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Interfaces/DataAccess/Repository/ManagementRepositories/Company/IShopRepository.cs
@@ -0,0 +1,9 @@
+﻿using JustCommerce.Application.Common.DataAccess.Repository;
+using JustCommerce.Domain.Entities.Company;
+
+namespace JustCommerce.Application.Common.Interfaces.DataAccess.Repository.ManagementRepositories.Company
+{
+    public interface IShopRepository : IBaseRepository<ShopEntity>
+    {
+    }
+}
