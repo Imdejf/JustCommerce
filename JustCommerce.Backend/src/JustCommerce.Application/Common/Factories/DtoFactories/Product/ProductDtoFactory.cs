7f8a870740b282f34d2394cd7aeba00940f87337
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-25 23:26:37 +0200

    Add ProductDtoFactory

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Factories/DtoFactories/Product/ProductDtoFactory.cs
@@ -0,0 +1,22 @@
+﻿using JustCommerce.Application.Common.DTOs.Product;
+using JustCommerce.Domain.Entities.Product;
+
+namespace JustCommerce.Application.Common.Factories.DtoFactories.Product
+{
+    public class ProductDtoFactory
+    {
+        public static ProductDTO CreateFromEntity(ProductEntity product)
+        {
+            return new ProductDTO
+            { 
+                Id = product.Id,
+                Slug = product.Slug,
+                Top = product.Top,
+                Newsletter = product.Newsletter,
+                Active = product.Active,
+                AvailabilityType = product.AvailabilityType,
+                ProductLang = product.ProductLang?.Select(c => ProductLangDtoFactory.CreateFromEntity(c)).ToArray(),
+            };
+        }
+    }
+}
