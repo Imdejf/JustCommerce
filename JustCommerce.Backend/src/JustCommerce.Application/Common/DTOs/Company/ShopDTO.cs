8e5259b926e42d5b96c43144faa5b0fdde571eae
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-25 23:26:48 +0200

    Add ShopDTO

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/DTOs/Company/ShopDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace JustCommerce.Application.Common.DTOs.Company
+{
+    public class ShopDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public bool Active { get; set; }
+        public string FullName { get; set; }
+        public string AddressLine { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Country { get; set; }
+        public string Email { get; set; }
+    }
+}
