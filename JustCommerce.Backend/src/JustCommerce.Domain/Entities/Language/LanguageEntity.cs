7110cb25bad6b911bfda65ea4af08dbe4c689453
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-26 21:05:57 +0200

    Add entity LanguageEntity

---
+++ JustCommerce.Backend/src/JustCommerce.Domain/Entities/Language/LanguageEntity.cs
@@ -0,0 +1,10 @@
+﻿using JustCommerce.Domain.Entities.Abstract;
+
+namespace JustCommerce.Domain.Entities.Language
+{
+    public sealed class LanguageEntity : AuditableEntity
+    {
+        public string IsoCode { get; set; }
+        public string Name { get; set; }
+    }
+}
