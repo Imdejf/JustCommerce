ca0b01a5f469e0f18de2c2e2ca9e8dee4ab5db12
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-26 21:06:09 +0200

    Add LanguageConfig

---
+++ JustCommerce.Backend/src/JustCommerce.Persistence/DataAccess/EntitiesConfig/Language/LanguageConfig.cs
@@ -0,0 +1,52 @@
+﻿using JustCommerce.Domain.Entities.Language;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustCommerce.Persistence.DataAccess.EntitiesConfig.Language
+{
+    internal sealed class LanguageConfig : IEntityTypeConfiguration<LanguageEntity>
+    {
+        public void Configure(EntityTypeBuilder<LanguageEntity> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.HasIndex(c => c.Id);
+
+            builder.Property(c => c.Id)
+                   .ValueGeneratedOnAdd();
+
+            builder.Property(c => c.Name)
+                   .HasColumnType("varchar")
+                   .HasMaxLength(50)
+                   .IsRequired();
+
+            builder.Property(c => c.IsoCode)
+                   .HasColumnType("varchar")
+                   .HasMaxLength(6)
+                   .IsRequired();
+
+            builder.Property(c => c.CreatedBy)
+             .HasColumnType("varchar")
+             .HasMaxLength(50)
+             .IsRequired();
+
+            builder.Property(c => c.CreatedDate)
+                   .HasColumnType("datetime")
+                   .IsRequired();
+
+            builder.Property(c => c.LastModifiedBy)
+                    .HasColumnType("varchar")
+                    .HasMaxLength(50)
+                    .IsRequired(false);
+
+            builder.Property(c => c.LastModifiedDate)
+                   .HasColumnType("datetime")
+                   .IsRequired(false);
+        }
+    }
+}
