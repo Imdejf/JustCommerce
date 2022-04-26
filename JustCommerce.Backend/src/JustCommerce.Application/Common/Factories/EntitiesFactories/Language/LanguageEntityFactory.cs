f3ee0e4ea87cafdd9748ff1ce5c03fad38d2ddc7
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-26 21:04:57 +0200

    Add LanguageEntityFactory

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Factories/EntitiesFactories/Language/LanguageEntityFactory.cs
@@ -0,0 +1,22 @@
+﻿using JustCommerce.Application.Features.ManagemenetFeatures.Language.Command;
+using JustCommerce.Domain.Entities.Language;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustCommerce.Application.Common.Factories.EntitiesFactories.Language
+{
+    public class LanguageEntityFactory
+    {
+        public static LanguageEntity CreateFromShopCommand(CreateLanguage.Command command)
+        {
+            return new LanguageEntity
+            {
+                Name = command.Name,
+                IsoCode = command.IsoCode,
+            };
+        }
+    }
+}
