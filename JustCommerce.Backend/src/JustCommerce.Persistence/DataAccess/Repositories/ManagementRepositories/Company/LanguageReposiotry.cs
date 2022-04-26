c9e53882344e7a321afe7e983fde6bbf97b79ffb
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-26 21:06:16 +0200

    Add LanguageReposiotry

---
+++ JustCommerce.Backend/src/JustCommerce.Persistence/DataAccess/Repositories/ManagementRepositories/Company/LanguageReposiotry.cs
@@ -0,0 +1,18 @@
+﻿using JustCommerce.Application.Common.Interfaces.DataAccess.Repository.ManagementRepositories.Company;
+using JustCommerce.Domain.Entities.Language;
+using Microsoft.EntityFrameworkCore;
+
+namespace JustCommerce.Persistence.DataAccess.Repositories.ManagementRepositories.Company
+{
+    internal sealed class LanguageReposiotry : BaseRepository<LanguageEntity>, ILanguageRepository
+    {
+        public LanguageReposiotry(DbSet<LanguageEntity> entity) : base(entity)
+        {
+        }
+
+        public Task<bool> ExistNameOrIsoCode(string name, string isoCode)
+        {
+            return _entity.AnyAsync(c => c.IsoCode == isoCode || c.Name == name);
+        }
+    }
+}
