34aa24ef1cd7e35c233b5fd491609d063c3060a1
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-26 21:05:02 +0200

    Add ILanguageRepository

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Interfaces/DataAccess/Repository/ManagementRepositories/Company/ILanguageRepository.cs
@@ -0,0 +1,10 @@
+﻿using JustCommerce.Application.Common.DataAccess.Repository;
+using JustCommerce.Domain.Entities.Language;
+
+namespace JustCommerce.Application.Common.Interfaces.DataAccess.Repository.ManagementRepositories.Company
+{
+    public interface ILanguageRepository : IBaseRepository<LanguageEntity>
+    {
+        Task<bool> ExistNameOrIsoCode(string name, string isoCode);
+    }
+}
