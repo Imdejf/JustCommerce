a406c95763c3bfe2b7bfddb2f0c1386d48349599
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-26 21:05:50 +0200

    Add implementation GetLanguage

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Features/ManagemenetFeatures/Language/Query/GetLanguage.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+using JustCommerce.Application.Common.DataAccess.Repository;
+using JustCommerce.Application.Common.DTOs.Language;
+using JustCommerce.Application.Common.Factories.DtoFactories.Language;
+using JustCommerce.Application.Common.Interfaces;
+
+namespace JustCommerce.Application.Features.ManagemenetFeatures.Language.Query
+{
+    public static class GetLanguage
+    {
+
+        public sealed record Query() : IRequestWrapper<List<LanguageDTO>>;
+
+        public sealed class Handler : IRequestHandlerWrapper<Query, List<LanguageDTO>>
+        {
+            private readonly IUnitOfWorkManagmenet _unitOfWorkManagmenet;
+            public Handler(IUnitOfWorkManagmenet unitOfWorkManagmenet)
+            {
+                _unitOfWorkManagmenet = unitOfWorkManagmenet;
+            }
+
+            public async Task<List<LanguageDTO>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var languageList = await _unitOfWorkManagmenet.Language.GetAllAsync(cancellationToken);
+
+                return languageList.Select(c => LanguageDtoFactory.CreateFromEntity(c)).ToList();
+            }
+        }
+
+        public sealed class Validator : AbstractValidator<Query>
+        {
+            public Validator()
+            {
+
+            }
+        }
+
+    }
+}
