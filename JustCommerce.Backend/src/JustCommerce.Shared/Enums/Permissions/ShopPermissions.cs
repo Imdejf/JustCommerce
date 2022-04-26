3ba8dca0bf451a13d16d7fb1021793798b8683cc
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-26 21:06:48 +0200

    Add ShopPermissions

---
+++ JustCommerce.Backend/src/JustCommerce.Shared/Enums/Permissions/ShopPermissions.cs
@@ -0,0 +1,11 @@
+﻿namespace E_Commerce.Shared.Enums.Permissions
+{
+    public enum ShopPermissions
+    {
+        Create = 1,
+        Edit = 2,
+        Delete = 4,
+        ViewList = 8,
+        Detail = 16,
+    }
+}
