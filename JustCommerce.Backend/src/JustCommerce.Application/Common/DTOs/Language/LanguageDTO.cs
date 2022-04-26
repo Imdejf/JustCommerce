8fce932b96d91acf05f2829050483710b812a373
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-26 21:04:45 +0200

    Add LanguageDTO

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/DTOs/Language/LanguageDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustCommerce.Application.Common.DTOs.Language
+{
+    public class LanguageDTO
+    {
+        public Guid Id { get; set; }
+        public string IsoCode { get; set; }
+        public string Name { get; set; }
+    }
+}
