ce0c8a94c3cab9764884624e090ccb9dca4ff230
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-26 22:18:17 +0200

    Add DI for ftp

---
+++ JustCommerce.Backend/src/JustCommerce.Infrastructure/DependencyInjection/AddCustomeServices.cs
@@ -0,0 +1,47 @@
+﻿using DataSharp.FtpFileManagement.DependencyInjection;
+using JustCommerce.Application.Common.Interfaces;
+using JustCommerce.Infrastructure.Configurations;
+using JustCommerce.Infrastructure.Implementations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JustCommerce.Infrastructure.DependencyInjection
+{
+    public static partial class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddFtpFileManager(this IServiceCollection services, IConfigurationSection configurationSection)
+        {
+            FtpFileConfig config = new FtpFileConfig();
+            configurationSection.Bind(config);
+
+            services.AddFtpFileManager(c => {
+                c.AddConnection(config.Key, a =>
+                     a.WithHost(config.Host)
+                      .WithCredentails(config.UserId, config.Password)
+                      .UsingRootCatalog(config.UsingRootCatalog)
+                      .EnableSSL(config.EnableSSL)
+                      .UsePassive(config.UsePassive)
+                      .KeepAlive(config.KeepAlive)
+                      .WaitForReadWriteOperationExecutionFor(config.WaitForRequestExecutionFor)
+                      .MaximumNumberOfFilesThatCanBeRemovedAtOnce(config.MaximumNumberOfFilesThatCanBeRemovedAtOnce)
+                      .WaitForRequestExecutionFor(config.WaitForRequestExecutionFor)
+                      .OnDomainName(config.OnDomainName)
+
+                );
+            });
+            services.AddTransient<IFtpFileManager, FtpFileManager>();
+            return services;
+        }
+        public static IServiceCollection CreateFtpFolder(this IServiceCollection services)
+        {
+            DataSharp.FtpFileManagement.Interfaces.IFtpFileManager ftpFileManager = services.BuildServiceProvider().GetRequiredService<DataSharp.FtpFileManagement.Interfaces.IFtpFileManager>();
+            var currentConnection = ftpFileManager.GetCurrentConnection();
+            if (!ftpFileManager.DirectoryExists(currentConnection.Value.RootFolder))
+            {
+                ftpFileManager.CreateDirectory(currentConnection.Value.RootFolder);
+            }
+
+            return services;
+        }
+    }
+}
