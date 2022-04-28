b4b547e6b359dc6365bbc0ac11bdad5306951bd4
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-28 23:13:24 +0200

    Add implementation WatermarkManager

---
+++ JustCommerce.Backend/src/JustCommerce.Infrastructure/DependencyInjection/WatermarkManagerServices.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Watermark.DependencyInjection;
+
+namespace JustCommerce.Infrastructure.DependencyInjection
+{
+    public static partial class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddWatermarkManager(this IServiceCollection services, IConfigurationSection configurationSection)
+        {
+            services.AddWatermark();
+
+            services.AddConfigForImageWatermark(new Watermark.Configs.WatermarkImageConfig()
+            {
+                Operation = Watermark.Enums.WatermarkOperation.AddPicture | Watermark.Enums.WatermarkOperation.AddText
+            });
+
+            services.AddConfigForFileWatermark(new Watermark.Configs.WatermarkTextFileConfig()
+            {
+                Operation = Watermark.Enums.WatermarkOperation.AddPicture | Watermark.Enums.WatermarkOperation.AddText
+            });
+
+            return services;
+        }
+    }
+}
