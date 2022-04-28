1ad4258837f1731876ae57a15120a268559f7057
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-04-28 23:13:08 +0200

    Add IWatermarkManager

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Interfaces/IWatermarkManager.cs
@@ -0,0 +1,9 @@
+﻿using JustCommerce.Shared.Models;
+
+namespace JustCommerce.Application.Common.Interfaces
+{
+    public interface IWatermarkManager
+    {
+        Task<Base64File> AddWatermarkToPicture(Base64File base64Picture, CancellationToken cancellationToken = default);
+    }
+}
