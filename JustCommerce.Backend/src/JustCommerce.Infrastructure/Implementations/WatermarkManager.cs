bbd9f72a8bee79385c01b20efe3eedca31112c3e
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-03 23:37:43 +0200

    Add WatermarkManager

---
+++ JustCommerce.Backend/src/JustCommerce.Infrastructure/Implementations/WatermarkManager.cs
@@ -0,0 +1,24 @@
+﻿using JustCommerce.Application.Common.Interfaces;
+using JustCommerce.Shared.Models;
+using Watermark.Interfaces.WatermarkGenerator;
+
+namespace JustCommerce.Infrastructure.Implementations
+{
+    public class WatermarkManager : IWatermarkManager
+    {
+        private readonly IWatermarkGenerator _wrapper;
+
+        public WatermarkManager(IWatermarkGenerator wrapper)
+        {
+            _wrapper = wrapper;
+        }
+
+        public async Task<Base64File> AddWatermarkToPicture(Base64File base64Picture, CancellationToken cancellationToken = default)
+        {
+            var base64 = await _wrapper.OnFile(base64Picture.Base64String)
+                                      .AsBase64FileAsync(cancellationToken);
+
+           return new Base64File(base64);
+        }
+    }
+}
