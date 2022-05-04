007e7eef3c01e888ff71c1e38839f784f2574a58
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-04 21:35:34 +0200

    Add ProductSellableEntity

---
+++ JustCommerce.Backend/src/JustCommerce.Domain/Entities/Product/ProductSellableEntity.cs
@@ -0,0 +1,22 @@
+﻿using JustCommerce.Domain.Entities.Abstract;
+using JustCommerce.Domain.Enums;
+
+namespace JustCommerce.Domain.Entities.Product
+{
+    public sealed class ProductSellableEntity : AuditableEntity
+    {
+        public Guid ProductId { get; set; }
+        public ProductEntity Product { get; set; }
+        public Currency Currency { get; set; }
+        public ProductColor ProductColor { get; set; }
+        public decimal NettoPrice { get; set; }
+        public decimal Tax { get; set; }
+        public decimal GrossPrice { get; set; }
+        public decimal OldPrice { get; set; }
+        public decimal ProducentPrice { get; set; }
+        public decimal Weight { get; set; }
+        public string ProductNumber { get; set; }
+        public string EanCode { get; set; }
+        public string IconPath { get; set; }
+    }
+}
