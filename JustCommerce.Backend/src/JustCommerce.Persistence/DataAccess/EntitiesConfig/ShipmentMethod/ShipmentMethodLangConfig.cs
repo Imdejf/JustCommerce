bad3d3084ee65a3647c3b8dd09aab368d121353e
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-06 12:24:51 +0200

    Add ShipmentMethodLangConfig

---
+++ JustCommerce.Backend/src/JustCommerce.Persistence/DataAccess/EntitiesConfig/ShipmentMethod/ShipmentMethodLangConfig.cs
@@ -0,0 +1,32 @@
+﻿using JustCommerce.Domain.Entities.ShipmentMethod;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustCommerce.Persistence.DataAccess.EntitiesConfig.ShipmentMethod
+{
+    internal sealed class ShipmentMethodLangConfig : IEntityTypeConfiguration<ShipmentMethodLangEntity>
+    {
+        public void Configure(EntityTypeBuilder<ShipmentMethodLangEntity> builder)
+        {
+            builder.HasKey(c => new { c.ShipmentMethodId, c.LangaugeId });
+
+            builder.HasOne(c => c.Language)
+                   .WithOne()
+                   .HasForeignKey<ShipmentMethodLangEntity>(c => c.LangaugeId);
+
+            builder.HasOne(c => c.ShipmentMethod)
+                   .WithMany(c => c.ShipmentMethodLang)
+                   .HasForeignKey(c => c.ShipmentMethodId);
+
+            builder.Property(c => c.Name)
+                   .HasColumnType("varchar")
+                   .HasMaxLength(100)
+                   .IsRequired(true);
+        }
+    }
+}
