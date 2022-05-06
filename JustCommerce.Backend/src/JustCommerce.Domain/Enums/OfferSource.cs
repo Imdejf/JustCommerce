9937ae5155cc05574e0f43e335cd3ad75486ed03
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-06 12:24:24 +0200

    Add OfferSource

---
+++ JustCommerce.Backend/src/JustCommerce.Domain/Enums/OfferSource.cs
@@ -0,0 +1,11 @@
+﻿namespace JustCommerce.Domain.Enums
+{
+    public enum OfferSource
+    {
+        Email = 0,
+        Phone = 1,
+        Chat = 2,
+        Allegro = 3,
+        RegularCustomer = 4,
+    }
+}
