4021ddecc4bdbc02251a834c3294a9396adc7f85
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-06 12:24:13 +0200

    Add ShipmentMethodLangEntity

---
+++ JustCommerce.Backend/src/JustCommerce.Domain/Entities/ShipmentMethod/ShipmentMethodLangEntity.cs
@@ -0,0 +1,13 @@
+﻿using JustCommerce.Domain.Entities.Language;
+
+namespace JustCommerce.Domain.Entities.ShipmentMethod
+{
+    public sealed class ShipmentMethodLangEntity
+    {
+        public Guid ShipmentMethodId { get; set; }
+        public ShipmentMethodEntity ShipmentMethod { get; set; }
+        public Guid LangaugeId { get; set; }
+        public LanguageEntity Language { get; set; }
+        public string Name { get; set; }
+    }
+}
