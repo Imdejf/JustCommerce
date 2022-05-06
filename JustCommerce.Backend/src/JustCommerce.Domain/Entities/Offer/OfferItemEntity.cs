9e1ac17f0eb1addd50cc18239ffdaf69475ce651
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-06 12:23:59 +0200

    Add OfferItemEntity

---
+++ JustCommerce.Backend/src/JustCommerce.Domain/Entities/Offer/OfferItemEntity.cs
@@ -0,0 +1,18 @@
+﻿using JustCommerce.Domain.Entities.Abstract;
+using JustCommerce.Domain.Entities.Product;
+
+namespace JustCommerce.Domain.Entities.Offer
+{
+    public sealed class OfferItemEntity
+    {
+        public Guid OfferId { get; set; }
+        public OfferEntity Offer { get; set; }
+        public Guid ProductSellableId { get; set; }
+        public ProductSellableEntity ProductSellable { get; set; }
+        public int Quantity { get; set; }
+        public decimal NettoPrice { get; set; }
+        public decimal Tax { get; set; }
+        public decimal GrossPrice { get; set; }
+        public decimal ProducentPrice { get; set; }
+    }
+}
