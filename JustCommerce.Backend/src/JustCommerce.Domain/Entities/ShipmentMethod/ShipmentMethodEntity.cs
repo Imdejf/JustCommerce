8a29d910394a570e1da815f49ec551666267f397
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-06 12:24:08 +0200

    Add ShipmentMethodEntity

---
+++ JustCommerce.Backend/src/JustCommerce.Domain/Entities/ShipmentMethod/ShipmentMethodEntity.cs
@@ -0,0 +1,10 @@
+﻿using JustCommerce.Domain.Entities.Abstract;
+
+namespace JustCommerce.Domain.Entities.ShipmentMethod
+{
+    public class ShipmentMethodEntity : AuditableEntity
+    {
+        public int OrderValue { get; set; }
+        public ICollection<ShipmentMethodLangEntity> ShipmentMethodLang { get; set; }
+    }
+}
