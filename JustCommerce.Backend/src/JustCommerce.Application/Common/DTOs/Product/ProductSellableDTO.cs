8d67f44bd77878db45aa8468feb0027521879499
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-06 12:23:33 +0200

    Add ProductSellableDTO

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/DTOs/Product/ProductSellableDTO.cs
@@ -0,0 +1,23 @@
+﻿using JustCommerce.Domain.Enums;
+using JustCommerce.Shared.Models;
+
+namespace JustCommerce.Application.Common.DTOs.Product
+{
+    public class ProductSellableDTO
+    {
+        public Guid ProductId { get; set; }
+        public Currency Currency { get; set; }
+        public ProductColor ProductColor { get; set; }
+        public decimal NettoPrice { get; set; }
+        public decimal Tax { get; set; }
+        public decimal GrossPrice { get; set; }
+        public decimal OldPrice { get; set; }
+        public decimal ProducentPrice { get; set; }
+        public decimal Weight { get; set; }
+        public string ProductNumber { get; set; }
+        public string EanCode { get; set; }
+        public string? IconPath { get; set; }
+        public Base64File? Base64File { get; set; }
+        public string? FileName { get; set; }
+    }
+}
