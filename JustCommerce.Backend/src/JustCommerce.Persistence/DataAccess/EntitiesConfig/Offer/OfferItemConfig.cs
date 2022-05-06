77a28d59344d1f3c42898e712db87ad3deb06a3f
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-06 12:24:38 +0200

    Add OfferItemConfig

---
+++ JustCommerce.Backend/src/JustCommerce.Persistence/DataAccess/EntitiesConfig/Offer/OfferItemConfig.cs
@@ -0,0 +1,47 @@
+﻿using JustCommerce.Domain.Entities.Offer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustCommerce.Persistence.DataAccess.EntitiesConfig.Offer
+{
+    internal sealed class OfferItemConfig : IEntityTypeConfiguration<OfferItemEntity>
+    {
+        public void Configure(EntityTypeBuilder<OfferItemEntity> builder)
+        {
+            builder.ToTable("OfferItem");
+
+            builder.HasKey(c => new { c.OfferId, c.ProductSellableId });
+
+            builder.HasOne(c => c.Offer)
+                .WithMany(c => c.OfferItem)
+                .HasForeignKey(c => c.OfferId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(c => c.ProductSellable)
+                .WithMany(c => c.OfferItem)
+                .HasForeignKey(c => c.ProductSellableId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Property(c => c.Quantity)
+                   .HasColumnType("int")
+                   .IsRequired(true);
+
+            builder.Property(c => c.NettoPrice)
+                   .HasColumnType("decimal");
+
+            builder.Property(c => c.GrossPrice)
+                   .HasColumnType("decimal");
+
+            builder.Property(c => c.Tax)
+                   .HasColumnType("decimal");
+
+            builder.Property(c => c.ProducentPrice)
+                   .HasColumnType("decimal");
+        }
+    }
+}
