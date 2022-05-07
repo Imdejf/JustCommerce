8f4177a9e20dd182ca217a01a1d3505a9a6e5712
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-07 19:24:55 +0200

    Add implementation for GetOffer

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Features/AdministrationFeatures/Offer/Query/GetOffer.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using JustCommerce.Application.Common.DataAccess.Repository;
+using JustCommerce.Application.Common.DTOs.Offer;
+using JustCommerce.Application.Common.Factories.DtoFactories.Offer;
+using JustCommerce.Application.Common.Interfaces;
+
+namespace JustCommerce.Application.Features.AdministrationFeatures.Offer.Query
+{
+    public static class GetOffer
+    {
+
+        public sealed record Query() : IRequestWrapper<List<OfferDTO>>;
+
+        public sealed class Handler : IRequestHandlerWrapper<Query, List<OfferDTO>>
+        {
+            private readonly IUnitOfWorkAdministration _unitOfWorkAdministration;
+            public Handler(IUnitOfWorkAdministration unitOfWorkAdministration)
+            {
+                _unitOfWorkAdministration = unitOfWorkAdministration;
+            }
+
+            public async Task<List<OfferDTO>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var offerList = await _unitOfWorkAdministration.Offer.GetAllAsync(cancellationToken);
+                return offerList.Select(c => OfferDtoFactory.CreateFromEntity(c)).ToList();
+            }
+        }
+
+        public sealed class Validator : AbstractValidator<Query>
+        {
+            public Validator()
+            {
+
+            }
+        }
+
+    }
+}
