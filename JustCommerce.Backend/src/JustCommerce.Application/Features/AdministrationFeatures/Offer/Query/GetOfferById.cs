26204089df269366ba1aabe5d4a8dc43e528f6a5
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-07 19:25:07 +0200

    Add implementation for GetOfferById

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Features/AdministrationFeatures/Offer/Query/GetOfferById.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+using JustCommerce.Application.Common.DataAccess.Repository;
+using JustCommerce.Application.Common.DTOs.Offer;
+using JustCommerce.Application.Common.Factories.DtoFactories.Offer;
+using JustCommerce.Application.Common.Interfaces;
+using MediatR;
+
+namespace JustCommerce.Application.Features.AdministrationFeatures.Offer.Query
+{
+    public static class GetOfferById
+    {
+
+        public sealed record Query(Guid OfferId) : IRequestWrapper<OfferDTO>;
+
+        public sealed class Handler : IRequestHandlerWrapper<Query, OfferDTO>
+        {
+            private readonly IUnitOfWorkAdministration _unitOfWorkAdministration;
+            public Handler(IUnitOfWorkAdministration unitOfWorkAdministration)
+            {
+                _unitOfWorkAdministration = unitOfWorkAdministration;
+            }
+
+            public async Task<OfferDTO> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var offer = await _unitOfWorkAdministration.Offer.GetByIdAsync(request.OfferId, cancellationToken);
+                return OfferDtoFactory.CreateFromEntity(offer);
+            }
+        }
+
+        public sealed class Validator : AbstractValidator<Query>
+        {
+            public Validator()
+            {
+                RuleFor(c => c.OfferId).NotEqual(Guid.Empty);
+            }
+        }
+
+    }
+}
