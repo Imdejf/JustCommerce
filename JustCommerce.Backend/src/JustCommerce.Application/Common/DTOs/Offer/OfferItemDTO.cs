238b86f5a69bd9a94de895b09975651258340a9e
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-07 19:26:15 +0200

    Add OfferItemDTO

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/DTOs/Offer/OfferItemDTO.cs
@@ -0,0 +1,15 @@
+﻿using JustCommerce.Application.Common.DTOs.Product;
+
+namespace JustCommerce.Application.Common.DTOs.Offer
+{
+    public class OfferItemDTO
+    {
+        public Guid OfferId { get; set; }
+        public Guid ProductSellableId { get; set; }
+        public int Quantity { get; set; }
+        public decimal NettoPrice { get; set; }
+        public decimal Tax { get; set; }
+        public decimal GrossPrice { get; set; }
+        public decimal ProducentPrice { get; set; }
+    }
+}
