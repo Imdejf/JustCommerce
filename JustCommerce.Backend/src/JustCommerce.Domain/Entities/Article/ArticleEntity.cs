a6c45e0e69daf3abf893c54d2d9c0e09e049b972
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-08 21:36:32 +0200

    Add ArticleEntity

---
+++ JustCommerce.Backend/src/JustCommerce.Domain/Entities/Article/ArticleEntity.cs
@@ -0,0 +1,12 @@
+﻿using JustCommerce.Domain.Entities.Abstract;
+
+namespace JustCommerce.Domain.Entities.Article
+{
+    public sealed class ArticleEntity : AuditableEntity
+    {
+        public string Slug { get; set; }
+        public string IconPath { get; set; }
+        public bool Active { get; set; }
+        public ICollection<ArticleLangEntity> ArticleLang { get; set; }
+    }
+}
