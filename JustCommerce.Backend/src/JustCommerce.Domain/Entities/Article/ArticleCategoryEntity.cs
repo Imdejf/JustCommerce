03517d768993a273f5aca2a90236c2ce4c372195
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-08 21:37:59 +0200

    Add ArticleCategoryEntity

---
+++ JustCommerce.Backend/src/JustCommerce.Domain/Entities/Article/ArticleCategoryEntity.cs
@@ -0,0 +1,12 @@
+﻿using JustCommerce.Domain.Entities.Abstract;
+
+namespace JustCommerce.Domain.Entities.Article
+{
+    public sealed class ArticleCategoryEntity : AuditableEntity
+    {
+        public string Slug { get; set; }
+        public string IconPath { get; set; }
+        public bool Active { get; set; }
+        public ICollection<ArticleCategoryLangEntity> ArticleCategoryLang { get; set; }
+    }
+}
