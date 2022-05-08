4d7aa94b4bd1417640621352d8f9b0787e53797a
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-08 21:37:48 +0200

    Add ArticleRelatedProduct

---
+++ JustCommerce.Backend/src/JustCommerce.Domain/Entities/Article/ArticleRelatedProduct.cs
@@ -0,0 +1,12 @@
+﻿using JustCommerce.Domain.Entities.Product;
+
+namespace JustCommerce.Domain.Entities.Article
+{
+    public class ArticleRelatedProduct
+    {
+        public Guid ArticleId { get; set; }
+        public ArticleEntity Article { get; set; }
+        public Guid ProductId { get; set; }
+        public ProductEntity ProductEntity { get; set; }
+    }
+}
