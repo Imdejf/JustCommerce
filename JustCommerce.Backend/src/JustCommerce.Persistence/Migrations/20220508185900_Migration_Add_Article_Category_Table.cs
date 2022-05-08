c6b0f7c47625116d7ca77ec29196a97987eb3886
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-08 21:35:36 +0200

    Create migration Migration_Add_Article_Category_Table

---
+++ JustCommerce.Backend/src/JustCommerce.Persistence/Migrations/20220508185900_Migration_Add_Article_Category_Table.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JustCommerce.Persistence.Migrations
+{
+    public partial class Migration_Add_Article_Category_Table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ArticleCategory",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Slug = table.Column<string>(type: "varchar(180)", maxLength: 180, nullable: false),
+                    IconPath = table.Column<string>(type: "varchar(180)", maxLength: 180, nullable: true),
+                    Active = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedBy = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime", nullable: false),
+                    LastModifiedBy = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: true),
+                    LastModifiedDate = table.Column<DateTime>(type: "datetime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArticleCategory", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ArticleCategoryLang",
+                columns: table => new
+                {
+                    LanguageId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ArticleCategoryId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "varchar(150)", maxLength: 150, nullable: false),
+                    MetaTitle = table.Column<string>(type: "varchar(150)", maxLength: 150, nullable: true),
+                    MetaDescription = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: true),
+                    Content = table.Column<string>(type: "varchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArticleCategoryLang", x => new { x.ArticleCategoryId, x.LanguageId });
+                    table.ForeignKey(
+                        name: "FK_ArticleCategoryLang__Language_LanguageId",
+                        column: x => x.LanguageId,
+                        principalTable: "_Language",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ArticleCategoryLang_ArticleCategory_ArticleCategoryId",
+                        column: x => x.ArticleCategoryId,
+                        principalTable: "ArticleCategory",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "ProductType",
+                keyColumn: "Id",
+                keyValue: new Guid("371b379b-3ff4-4523-bd15-b545e185b97c"),
+                column: "CreatedDate",
+                value: new DateTime(2022, 5, 8, 20, 58, 58, 172, DateTimeKind.Local).AddTicks(8799));
+
+            migrationBuilder.UpdateData(
+                table: "ProductType",
+                keyColumn: "Id",
+                keyValue: new Guid("8539176a-3578-48e1-a2d7-3cce1389c762"),
+                column: "CreatedDate",
+                value: new DateTime(2022, 5, 8, 20, 58, 58, 172, DateTimeKind.Local).AddTicks(8697));
+
+            migrationBuilder.UpdateData(
+                table: "ProductType",
+                keyColumn: "Id",
+                keyValue: new Guid("d13b4109-2acd-46f5-bd8b-d7f789fac0e0"),
+                column: "CreatedDate",
+                value: new DateTime(2022, 5, 8, 20, 58, 58, 172, DateTimeKind.Local).AddTicks(8807));
+
+            migrationBuilder.UpdateData(
+                table: "ProductType",
+                keyColumn: "Id",
+                keyValue: new Guid("e8d5076f-7501-4bb7-8581-f7f894d7a879"),
+                column: "CreatedDate",
+                value: new DateTime(2022, 5, 8, 20, 58, 58, 172, DateTimeKind.Local).AddTicks(8814));
+
+            migrationBuilder.UpdateData(
+                table: "Shop",
+                keyColumn: "Id",
+                keyValue: new Guid("6cef7328-534d-4699-98af-8779fba7d3a1"),
+                column: "CreatedDate",
+                value: new DateTime(2022, 5, 8, 20, 58, 58, 194, DateTimeKind.Local).AddTicks(2771));
+
+            migrationBuilder.UpdateData(
+                table: "Shop",
+                keyColumn: "Id",
+                keyValue: new Guid("83e84e94-b1ee-4cbf-be40-daea69347600"),
+                column: "CreatedDate",
+                value: new DateTime(2022, 5, 8, 20, 58, 58, 194, DateTimeKind.Local).AddTicks(2703));
+
+            migrationBuilder.UpdateData(
+                table: "_Language",
+                keyColumn: "Id",
+                keyValue: new Guid("87f0f759-323f-477d-886a-0afd7272ccd9"),
+                column: "CreatedDate",
+                value: new DateTime(2022, 5, 8, 20, 58, 58, 125, DateTimeKind.Local).AddTicks(4773));
+
+            migrationBuilder.UpdateData(
+                table: "_Language",
+                keyColumn: "Id",
+                keyValue: new Guid("b80fe531-4bb7-4f2d-a69e-86d8e20602e9"),
+                column: "CreatedDate",
+                value: new DateTime(2022, 5, 8, 20, 58, 58, 120, DateTimeKind.Local).AddTicks(801));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArticleCategory_Id",
+                table: "ArticleCategory",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArticleCategoryLang_LanguageId",
+                table: "ArticleCategoryLang",
+                column: "LanguageId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ArticleCategoryLang");
+
+            migrationBuilder.DropTable(
+                name: "ArticleCategory");
+
+            migrationBuilder.UpdateData(
+                table: "ProductType",
+                keyColumn: "Id",
+                keyValue: new Guid("371b379b-3ff4-4523-bd15-b545e185b97c"),
+                column: "CreatedDate",
+                value: new DateTime(2022, 5, 8, 18, 33, 42, 213, DateTimeKind.Local).AddTicks(6568));
+
+            migrationBuilder.UpdateData(
+                table: "ProductType",
+                keyColumn: "Id",
+                keyValue: new Guid("8539176a-3578-48e1-a2d7-3cce1389c762"),
+                column: "CreatedDate",
+                value: new DateTime(2022, 5, 8, 18, 33, 42, 213, DateTimeKind.Local).AddTicks(6457));
+
+            migrationBuilder.UpdateData(
+                table: "ProductType",
+                keyColumn: "Id",
+                keyValue: new Guid("d13b4109-2acd-46f5-bd8b-d7f789fac0e0"),
+                column: "CreatedDate",
+                value: new DateTime(2022, 5, 8, 18, 33, 42, 213, DateTimeKind.Local).AddTicks(6577));
+
+            migrationBuilder.UpdateData(
+                table: "ProductType",
+                keyColumn: "Id",
+                keyValue: new Guid("e8d5076f-7501-4bb7-8581-f7f894d7a879"),
+                column: "CreatedDate",
+                value: new DateTime(2022, 5, 8, 18, 33, 42, 213, DateTimeKind.Local).AddTicks(6585));
+
+            migrationBuilder.UpdateData(
+                table: "Shop",
+                keyColumn: "Id",
+                keyValue: new Guid("6cef7328-534d-4699-98af-8779fba7d3a1"),
+                column: "CreatedDate",
+                value: new DateTime(2022, 5, 8, 18, 33, 42, 240, DateTimeKind.Local).AddTicks(2598));
+
+            migrationBuilder.UpdateData(
+                table: "Shop",
+                keyColumn: "Id",
+                keyValue: new Guid("83e84e94-b1ee-4cbf-be40-daea69347600"),
+                column: "CreatedDate",
+                value: new DateTime(2022, 5, 8, 18, 33, 42, 240, DateTimeKind.Local).AddTicks(2517));
+
+            migrationBuilder.UpdateData(
+                table: "_Language",
+                keyColumn: "Id",
+                keyValue: new Guid("87f0f759-323f-477d-886a-0afd7272ccd9"),
+                column: "CreatedDate",
+                value: new DateTime(2022, 5, 8, 18, 33, 42, 161, DateTimeKind.Local).AddTicks(6506));
+
+            migrationBuilder.UpdateData(
+                table: "_Language",
+                keyColumn: "Id",
+                keyValue: new Guid("b80fe531-4bb7-4f2d-a69e-86d8e20602e9"),
+                column: "CreatedDate",
+                value: new DateTime(2022, 5, 8, 18, 33, 42, 156, DateTimeKind.Local).AddTicks(6886));
+        }
+    }
+}
