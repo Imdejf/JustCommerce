1677689429e0a531360254214f4e5e0495ad5b39
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-08 21:38:15 +0200

    Add ShipmentMethodLangDTO

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/DTOs/ShipmentMethod/ShipmentMethodLangDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace JustCommerce.Application.Common.DTOs.ShipmentMethod
+{
+    public class ShipmentMethodLangDTO
+    {
+        public Guid ShipmentMethodId { get; set; }
+        public Guid LangaugeId { get; set; }
+        public string Name { get; set; }
+    }
+}
