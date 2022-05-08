8ac0647fe0911751af42b5d20dabb2101c243af0
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-08 21:36:24 +0200

    Add ArticleEntity

---
+++ JustCommerce.Backend/src/JustCommerce.Domain/Entities/Article/ArticleLangEntity.cs
@@ -0,0 +1,18 @@
+﻿using JustCommerce.Domain.Entities.Language;
+
+namespace JustCommerce.Domain.Entities.Article
+{
+    public sealed class ArticleLangEntity
+    {
+        public Guid ArticleId { get; set; }
+        public ArticleEntity Article { get; set; }
+        public Guid LanguageId { get; set; }
+        public LanguageEntity Language { get; set; }
+        public string Title { get; set; }
+        public string ShortContent { get; set; }
+        public string KeyWords { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+        public string MetaDescription { get; set; }
+    }
+}
