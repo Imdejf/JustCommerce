b1c68462c71eb735c62973694f066e8eaf2edcce
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-10 23:57:02 +0200

    Add ArticleCategoryRelatedDTO

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/DTOs/Article/ArticleCategoryRelatedDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace JustCommerce.Application.Common.DTOs.Article
+{
+    public class ArticleCategoryRelatedDTO
+    {
+        public Guid ArticleId { get; set; }
+        public Guid CategoryId { get; set; }
+    }
+}
