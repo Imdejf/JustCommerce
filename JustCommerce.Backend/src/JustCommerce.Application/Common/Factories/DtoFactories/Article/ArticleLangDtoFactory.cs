f94028916a2e1ee159bfd663b95575a357511219
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-10 23:57:20 +0200

    Add ArticleLangDtoFactory

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Factories/DtoFactories/Article/ArticleLangDtoFactory.cs
@@ -0,0 +1,23 @@
+﻿using JustCommerce.Application.Common.DTOs.Article;
+using JustCommerce.Domain.Entities.Article;
+
+namespace JustCommerce.Application.Common.Factories.DtoFactories.Article
+{
+    public static class ArticleLangDtoFactory
+    {
+        public static ArticleLangDTO CreateFromEntity(ArticleLangEntity entity)
+        {
+            return new ArticleLangDTO
+            {
+                ShortContent = entity.ShortContent,
+                ArticleId = entity.ArticleId,
+                Content = entity.Content,
+                Description = entity.Description,
+                KeyWords = entity.KeyWords,
+                LanguageId = entity.LanguageId,
+                MetaDescription = entity.MetaDescription,
+                Title = entity.Title,
+            };
+        }
+    }
+}
