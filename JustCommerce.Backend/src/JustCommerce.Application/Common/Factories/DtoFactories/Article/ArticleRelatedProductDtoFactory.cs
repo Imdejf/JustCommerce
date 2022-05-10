6e0a8008ffbed5a07663f993cb3f5202b398f862
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-10 23:57:25 +0200

    Add ArticleRelatedProductDtoFactory

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Factories/DtoFactories/Article/ArticleRelatedProductDtoFactory.cs
@@ -0,0 +1,17 @@
+﻿using JustCommerce.Application.Common.DTOs.Article;
+using JustCommerce.Domain.Entities.Article;
+
+namespace JustCommerce.Application.Common.Factories.DtoFactories.Article
+{
+    public static class ArticleRelatedProductDtoFactory
+    {
+        public static ArticleProductRelatedDTO CreateFromEntity(ArticleRelatedProductEntity entity)
+        {
+            return new ArticleProductRelatedDTO
+            {
+                ArticleId = entity.ArticleId,
+                ProductId = entity.ProductId,
+            };
+        }
+    }
+}
