41b18a81c0075347441329ce92c626e7b9fb6318
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-10 23:55:56 +0200

    Add ArticleCategoryRelatedConfig

---
+++ JustCommerce.Backend/src/JustCommerce.Persistence/DataAccess/EntitiesConfig/Article/ArticleCategoryRelatedConfig.cs
@@ -0,0 +1,26 @@
+﻿using JustCommerce.Domain.Entities.Article;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace JustCommerce.Persistence.DataAccess.EntitiesConfig.Article
+{
+    internal sealed class ArticleCategoryRelatedConfig : IEntityTypeConfiguration<ArticleCategoryRelatedEntity>
+    {
+        public void Configure(EntityTypeBuilder<ArticleCategoryRelatedEntity> builder)
+        {
+            builder.ToTable("ArticleCategoryRelated");
+
+            builder.HasKey(c => new { c.ArticleId, c.CategoryId });
+
+            builder.HasOne(c => c.Article)
+                .WithMany(c => c.ArticleCategoryRelated)
+                .HasForeignKey(c => c.ArticleId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(c => c.Category)
+                .WithMany(c => c.ArticleCategoryRelated)
+                .HasForeignKey(c => c.CategoryId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
