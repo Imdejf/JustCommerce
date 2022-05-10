a68ce8c4dfcd029d4f36d8df6c0db31bb06291f3
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-10 23:56:53 +0200

    Add ArticleLangDTO

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/DTOs/Article/ArticleLangDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace JustCommerce.Application.Common.DTOs.Article
+{
+    public sealed class ArticleLangDTO
+    {
+        public Guid ArticleId { get; set; }
+        public Guid LanguageId { get; set; }
+        public string Title { get; set; }
+        public string ShortContent { get; set; }
+        public string KeyWords { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+        public string MetaDescription { get; set; }
+    }
+}
