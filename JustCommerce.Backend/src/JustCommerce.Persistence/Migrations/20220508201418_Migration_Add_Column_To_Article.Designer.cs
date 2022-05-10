eebcf3a7d85c0db4d10e29e77c75ee585b87eef6
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-10 23:55:36 +0200

    Add new migration Migration_Add_Column_To_Article

--- JustCommerce.Backend/src/JustCommerce.Persistence/Migrations/20220508201418_Migration_Add_Column_To_Article.Designer.cs
+++ JustCommerce.Backend/src/JustCommerce.Persistence/Migrations/20220508201418_Migration_Add_Column_To_Article.Designer.cs
@@ -4,6 +4,7 @@ using JustCommerce.Persistence.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace JustCommerce.Persistence.Migrations
 {
     [DbContext(typeof(JustCommerceDbContext))]
-    partial class JustCommerceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220508201418_Migration_Add_Column_To_Article")]
+    partial class Migration_Add_Column_To_Article
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
