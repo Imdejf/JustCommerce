65bd1c172107b508b347ef618cfd4aa5e917391e
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-11 15:21:34 +0200

    Add ArticleCategoryEntityFactory

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Factories/EntitiesFactories/ArticleCategory/ArticleCategoryEntityFactory.cs
@@ -0,0 +1,33 @@
+﻿using JustCommerce.Application.Features.AdministrationFeatures.ArticleCategory.Command;
+using JustCommerce.Domain.Entities.Article;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustCommerce.Application.Common.Factories.EntitiesFactories.ArticleCategory
+{
+    public static class ArticleCategoryEntityFactory
+    {
+        public static ArticleCategoryEntity CreateFromProductCommand(CreateArticleCategory.Command command)
+        {
+            return new ArticleCategoryEntity
+            {
+                ShopId = command.ShopId,
+                Slug = command.Slug,
+                Active = command.Active,
+                IconPath = command.IconPath,
+                ArticleCategoryLang = command.ArticleCategoryLang.Select(c => ArticleCategoryLangEntityFactory.CreateFromDto(c)).ToArray(),
+                ArticleCategoryRelated = command.ArticleCategoryRelated.Select(c => new ArticleCategoryRelatedEntity
+                {
+                    Category = null,
+                    CategoryId = c.CategoryId,
+                    Article = null,
+                    ArticleId = Guid.Empty
+                    
+                }).ToArray()
+            };
+        }
+    }
+}
