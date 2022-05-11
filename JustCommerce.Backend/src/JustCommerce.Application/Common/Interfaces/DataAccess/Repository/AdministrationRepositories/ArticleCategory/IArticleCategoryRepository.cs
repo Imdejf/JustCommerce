f14153dd9cdfa576ac91e8f6c26c9d19d466f38f
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-11 15:21:46 +0200

    Add IArticleCategoryRepository

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Interfaces/DataAccess/Repository/AdministrationRepositories/ArticleCategory/IArticleCategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using JustCommerce.Application.Common.DataAccess.Repository;
+using JustCommerce.Domain.Entities.Article;
+
+namespace JustCommerce.Application.Common.Interfaces.DataAccess.Repository.AdministrationRepositories.ArticleCategory
+{
+    public interface IArticleCategoryRepository : IBaseRepository<ArticleCategoryEntity>
+    {
+        Task<bool> ExistSlugAsync(string slug, CancellationToken cancellationToken = default);
+        Task<ArticleCategoryEntity?> GetFullArticleCategoryAsync(Guid articleCategoryId, CancellationToken cancellationToken = default);
+
+    }
+}
