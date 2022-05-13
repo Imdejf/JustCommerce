e273c34af23829d8888b0bdc80f6689dd3e9236b
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-13 23:26:36 +0200

    Add EmailTemplateDTO

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/DTOs/Email/EmailTemplateDTO.cs
@@ -0,0 +1,17 @@
+﻿using JustCommerce.Domain.Enums;
+
+namespace JustCommerce.Application.Common.DTOs.Email
+{
+    public class EmailTemplateDTO
+    {
+        public Guid ShopId { get; set; }
+        public Guid EmailAccountId { get; set; }
+        public string Name { get; set; }
+        public string FilePath { get; set; }
+        public string Email { get; set; }
+        public string EmailName { get; set; }
+        public string Subject { get; set; }
+        public bool Active { get; set; }
+        public EmailType EmailType { get; set; }
+    }
+}
