a25986621503d4b4f0c0f5b98bf66725dd95f0d6
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-13 23:26:46 +0200

    Add FileTemplateDTO

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/DTOs/FileTemplate/FileTemplateDTO.cs
@@ -0,0 +1,10 @@
+﻿using JustCommerce.Shared.Models;
+
+namespace JustCommerce.Application.Common.DTOs.FileTemplate
+{
+    public class FileTemplateDTO
+    {
+        public string? HtmlTemplate { get; set; }
+        public Base64File? Base64File { get; set; }
+    }
+}
