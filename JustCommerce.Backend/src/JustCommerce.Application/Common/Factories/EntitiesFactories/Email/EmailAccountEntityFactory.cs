c09d12177c741082ce2ad4d03e9fe3f0d6f9d21f
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-13 23:27:07 +0200

    Add EmailAccountEntityFactory

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Factories/EntitiesFactories/Email/EmailAccountEntityFactory.cs
@@ -0,0 +1,36 @@
+﻿using JustCommerce.Application.Features.ManagemenetFeatures.EmailAccount.Command;
+using JustCommerce.Domain.Entities.Email;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustCommerce.Application.Common.Factories.EntitiesFactories.Email
+{
+    public static class EmailAccountEntityFactory
+    {
+        public static EmailAccountEntity CreateFromEmailAccountCommand(CreateEmailAccount.Command command)
+        {
+            return new EmailAccountEntity
+            {
+                EmailAddress = command.EmailAddress,
+                Name = command.Name,
+                ShopId = command.ShopId,
+                SmtpLogin = command.SmtpLogin,
+                SmtpPassword = command.SmtpPassword,
+                SmtpProt = command.SmtpPort,
+                SmtpServer = command.SmtpServer,
+                ImapLogin = command.ImapLogin,
+                ImapPassword = command.ImapPassword,
+                ImapPort = command.ImapPort,
+                ImapServer = command.ImapServer,
+                Pop3Login = command.Pop3Login,
+                Pop3Password = command.Pop3Password,
+                Pop3Prot = command.Pop3Prot,
+                Pop3Server = command.Pop3Server,
+                
+            };
+        }
+    }
+}
