128c4258bbe09824dd409fbb410369743383c318
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-14 22:14:00 +0200

    Add IEmailAccountRepository

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Interfaces/DataAccess/Repository/ManagementRepositories/Email/IEmailAccountRepository.cs
@@ -0,0 +1,10 @@
+﻿using JustCommerce.Application.Common.DataAccess.Repository;
+using JustCommerce.Domain.Entities.Email;
+
+namespace JustCommerce.Application.Common.Interfaces.DataAccess.Repository.ManagementRepositories.Email
+{
+    public interface IEmailAccountRepository : IBaseRepository<EmailAccountEntity>
+    {
+        Task<List<EmailAccountEntity>> GetAllByShopId(Guid shopId, CancellationToken cancellationToken = default);
+    }
+}
