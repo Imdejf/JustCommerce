c6fd67dfe92d219b95f53314fce61a1a1fd6ab6a
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-15 21:53:21 +0200

    Add EmailAccountRepository

---
+++ JustCommerce.Backend/src/JustCommerce.Persistence/DataAccess/Repositories/ManagementRepositories/Email/EmailAccountRepository.cs
@@ -0,0 +1,18 @@
+﻿using JustCommerce.Application.Common.Interfaces.DataAccess.Repository.ManagementRepositories.Email;
+using JustCommerce.Domain.Entities.Email;
+using Microsoft.EntityFrameworkCore;
+
+namespace JustCommerce.Persistence.DataAccess.Repositories.ManagementRepositories.Email
+{
+    internal class EmailAccountRepository : BaseRepository<EmailAccountEntity>, IEmailAccountRepository
+    {
+        public EmailAccountRepository(DbSet<EmailAccountEntity> entity) : base(entity)
+        {
+        }
+
+        public Task<List<EmailAccountEntity>> GetAllByShopId(Guid shopId, CancellationToken cancellationToken = default)
+        {
+            return _entity.Where(c => c.ShopId == shopId).ToListAsync(cancellationToken);
+        }
+    }
+}
