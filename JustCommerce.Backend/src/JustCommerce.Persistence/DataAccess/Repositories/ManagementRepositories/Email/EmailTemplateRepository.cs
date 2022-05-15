9887a2b2d3eafbd6c306d7bbc471ecd8e529dd60
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-15 21:53:37 +0200

    Add EmailTemplateRepository

---
+++ JustCommerce.Backend/src/JustCommerce.Persistence/DataAccess/Repositories/ManagementRepositories/Email/EmailTemplateRepository.cs
@@ -0,0 +1,19 @@
+﻿using JustCommerce.Application.Common.Interfaces.DataAccess.Repository.ManagementRepositories.Email;
+using JustCommerce.Domain.Entities.Email;
+using JustCommerce.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace JustCommerce.Persistence.DataAccess.Repositories.ManagementRepositories.Email
+{
+    internal class EmailTemplateRepository : BaseRepository<EmailTemplateEntity>, IEmailTemplateRepository
+    {
+        public EmailTemplateRepository(DbSet<EmailTemplateEntity> entity) : base(entity)
+        {
+        }
+
+        public Task<bool> ExistEmailTypeAsync(Guid shopId, EmailType emaiLType, CancellationToken cancellationToken = default)
+        {
+            return _entity.Where(c => c.EmailType == emaiLType && c.ShopId == shopId).AnyAsync(cancellationToken);
+        }
+    }
+}
