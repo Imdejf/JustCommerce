f6d476fca8bb4d78d939d0432af7cb3c5f7a0c6e
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-16 12:45:46 +0200

    Add DictionaryLangEntityFactory

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Factories/EntitiesFactories/Dictionary/DictionaryLangEntityFactory.cs
@@ -0,0 +1,18 @@
+﻿using JustCommerce.Application.Common.DTOs.Dictionary;
+using JustCommerce.Domain.Entities.Dictionary;
+
+namespace JustCommerce.Application.Common.Factories.EntitiesFactories.Dictionary
+{
+    public static class DictionaryLangEntityFactory
+    {
+        public static DictionaryLangEntity CreateFromProductCommand(DictionaryLangDTO lang)
+        {
+            return new DictionaryLangEntity
+            {
+                DictionaryId = lang.DictionaryId,
+                LanguageId = lang.LanguageId,
+                Text = lang.Text,
+            };
+        }
+    }
+}
