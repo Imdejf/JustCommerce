a92a9d8f386b37d99247e18738d1d9861bd5d9d4
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-16 12:30:19 +0200

    Add DictionaryDTO

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/DTOs/Dictionary/DictionaryDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace JustCommerce.Application.Common.DTOs.Dictionary
+{
+    public class DictionaryDTO
+    {
+        public Guid DictionaryTypeId { get; set; }
+        public string Name { get; set; }
+        public string Dictionary { get; set; }
+        public ICollection<DictionaryLangDTO> DictionaryLang { get; set; }
+    }
+}
