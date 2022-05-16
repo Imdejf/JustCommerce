a7733358e205d9a0b2ba97f2a0ec86fc431511ea
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-16 11:17:05 +0200

    Add DictionaryLangEntity

---
+++ JustCommerce.Backend/src/JustCommerce.Domain/Entities/Dictionary/DictionaryLangEntity.cs
@@ -0,0 +1,13 @@
+﻿using JustCommerce.Domain.Entities.Language;
+
+namespace JustCommerce.Domain.Entities.Dictionary
+{
+    public sealed class DictionaryLangEntity
+    {
+        public Guid DictionaryId { get; set; }
+        public DictionaryEntity Dictionary { get; set; }
+        public Guid LanguageId { get; set; }
+        public LanguageEntity Language { get; set; }
+        public string Text { get; set; }
+    }
+}
