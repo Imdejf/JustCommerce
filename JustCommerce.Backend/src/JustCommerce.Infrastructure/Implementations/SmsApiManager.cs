48562a69f536acdc272efe1a7462521770a534d1
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-16 09:19:13 +0200

    Add implementation SendSms

--- JustCommerce.Backend/src/JustCommerce.Infrastructure/Implementations/SmsApiManager.cs
+++ JustCommerce.Backend/src/JustCommerce.Infrastructure/Implementations/SmsApiManager.cs
@@ -1,16 +1,29 @@
 ﻿using JustCommerce.Application.Common.Interfaces;
 using JustCommerce.Infrastructure.Configurations;
 using Microsoft.Extensions.Options;
+using SMSApi.Api;
 
 namespace JustCommerce.Infrastructure.Implementations
 {
     internal sealed class SmsApiManager : ISmsApiManager
     {
-        private readonly FtpFileConfig _ftpFileConfig;
-        public SmsApiManager(IOptions<FtpFileConfig> ftpFileConfig)
+        private readonly SmsApiConfig _ftpFileConfig;
+        
+        public SmsApiManager(IOptions<SmsApiConfig> ftpFileConfig)
         {
             _ftpFileConfig ??= ftpFileConfig.Value;
         }
 
+        public void SendSms(string text, string to, string sender)
+        {
+            IClient client = new ClientOAuth(_ftpFileConfig.Token);
+            var smsApi = new SMSFactory(client);
+
+            var result = smsApi.ActionSend()
+                               .SetText(text)
+                               .SetTo(to)
+                               .SetSender(sender)
+                               .Execute;
+        }
     }
 }
