d616412eb39f4e2bea96ada935e1c145421011a6
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-16 11:17:15 +0200

    Add Sms

---
+++ JustCommerce.Backend/src/JustCommerce.Domain/Enums/Dictionary/Sms.cs
@@ -0,0 +1,16 @@
+﻿namespace JustCommerce.Domain.Enums.Dictionary
+{
+    public enum Sms
+    {
+        ResetPassword = 0,
+        OrderPlaced = 1,
+        OrderAccepted = 2,
+        OrderRejected = 3,
+        OrderConfirmed = 4,
+        OrderCancelled = 5,
+        OrderInProgress = 6,
+        OrderSend = 7,
+        Return = 8,
+        Completed = 9,
+    }
+}
