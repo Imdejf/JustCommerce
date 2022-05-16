36f18cbdd0809d86705ac0c214c3bae8db979cb8
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-16 11:16:58 +0200

    Add DictionaryEntity

---
+++ JustCommerce.Backend/src/JustCommerce.Domain/Entities/Dictionary/DictionaryEntity.cs
@@ -0,0 +1,14 @@
+﻿using JustCommerce.Domain.Entities.Abstract;
+using JustCommerce.Domain.Entities.Company;
+
+namespace JustCommerce.Domain.Entities.Dictionary
+{
+    public sealed class DictionaryEntity : AuditableEntity
+    {
+        public Guid DictionaryTypeId { get; set; }
+        public DictionaryTypeEntity DictionaryType { get; set; }
+        public string Name { get; set; }
+        public string Dictionary { get; set; }
+        ICollection<DictionaryLangEntity> DictionaryLang { get; set; }
+    }
+}
