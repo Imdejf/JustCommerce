c60b23ec2e8d71130b29b93959f5ab8da97d1450
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-16 12:13:55 +0200

    Add DictionaryTypeRepository

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Interfaces/DataAccess/Repository/ManagementRepositories/Dictionary/IDictionaryTypeRepository.cs
@@ -0,0 +1,10 @@
+﻿using JustCommerce.Application.Common.DataAccess.Repository;
+using JustCommerce.Domain.Entities.Dictionary;
+
+namespace JustCommerce.Application.Common.Interfaces.DataAccess.Repository.ManagementRepositories.Dictionary
+{
+    public interface DictionaryTypeRepository : IBaseRepository<DictionaryTypeEntity>
+    {
+
+    }
+}
