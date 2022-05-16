903b26c9ec37d2814b067efcd9aed8b588fbb42d
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-16 12:30:36 +0200

    Add DictionaryTypeEntityFactory

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Factories/EntitiesFactories/Dictionary/DictionaryTypeEntityFactory.cs
@@ -0,0 +1,18 @@
+﻿using JustCommerce.Domain.Entities.Dictionary;
+
+namespace JustCommerce.Application.Common.Factories.EntitiesFactories.Dictionary
+{
+    public static class DictionaryTypeEntityFactory
+    {
+        public static DictionaryTypeEntity CreateFromProductCommand(CreateDictionaryType.Command command)
+        {
+            return new DictionaryTypeEntity
+            {
+                Description = command.Description,
+                ShopId = command.ShopId,
+                DictionaryType = command.DictionaryType,
+                Name = command.Name,
+            };
+        }
+    }
+}
