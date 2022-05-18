d2b99af6497e548e0e22f710febfac56f01abb17
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-18 19:35:06 +0200

    Add OrderSource

---
+++ JustCommerce.Backend/src/JustCommerce.Domain/Enums/OrderSource.cs
@@ -0,0 +1,12 @@
+﻿namespace JustCommerce.Domain.Enums
+{
+    public enum OrderSource
+    {
+        None = 0,
+        Phone = 1,
+        Email = 2,
+        Chat = 3,
+        RegularCustomer = 4,
+        Platform = 5,
+    }
+}
