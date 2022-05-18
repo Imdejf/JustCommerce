8a2b81dbb42f4dea9d6212b3d331e8d5824978e1
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-18 19:35:11 +0200

    Add OrderStatus

---
+++ JustCommerce.Backend/src/JustCommerce.Domain/Enums/OrderStatus.cs
@@ -0,0 +1,15 @@
+﻿namespace JustCommerce.Domain.Enums
+{
+    public enum OrderStatus
+    {
+        Work = 0,
+        Placed = 1,
+        InProgress = 2,
+        Return = 3,
+        Clarify = 4,
+        Arrear = 5,
+        Cancel = 6,
+        Send = 7,
+        Completed= 8,
+    }
+}
