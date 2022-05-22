3194db439be486e2307b561b5e6c778bac97a532
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-22 18:11:10 +0200

    Add SmsAccountDTO

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/DTOs/Sms/SmsAccountDTO.cs
@@ -0,0 +1,13 @@
+﻿using JustCommerce.Domain.Enums;
+
+namespace JustCommerce.Application.Common.DTOs.Sms
+{
+    public class SmsAccountDTO
+    {
+        public SmsGate SmsGate { get; set; }
+        public string From { get; set; } = String.Empty;
+        public string? Token { get; set; }
+        public bool Test { get; set; }
+        public ICollection<SmsTemplateDTO>? SmsTemplate { get; set; }
+    }
+}
