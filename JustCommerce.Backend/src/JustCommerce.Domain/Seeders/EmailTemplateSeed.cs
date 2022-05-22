d61084c23dfa15f7061ea74555e1159677493143
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-22 18:13:00 +0200

    Add email seed

---
+++ JustCommerce.Backend/src/JustCommerce.Domain/Seeders/EmailTemplateSeed.cs
@@ -0,0 +1,15 @@
+﻿using JustCommerce.Domain.Common;
+using JustCommerce.Domain.Entities.Email;
+
+namespace JustCommerce.Domain.Seeders
+{
+    public static class EmailTemplateSeed
+    {
+        public static BaseSeed<EmailTemplateEntity> BaseSeed = new BaseSeed<EmailTemplateEntity>(SeedEmailTemplate);
+
+        private static IEnumerable<EmailTemplateEntity> SeedEmailTemplate => new List<EmailTemplateEntity>()
+        {
+            new EmailTemplateEntity("cf702e9b-cceb-476f-ba6e-455004ebf588","6cef7328-534d-4699-98af-8779fba7d3a1", "014e3b4b-e2a7-4499-8c82-a905b7b086e2", "EmailConfirmed", Directory.GetCurrentDirectory().Split("bin")[0] + $@"\Templates\EmailTemplates\6cef7328-534d-4699-98af-8779fba7d3a1\EmailConfirmation.html","kontakt@emagazynowo.pl", "eMagazynowo", "eMagazynowo : Potwierdzenie rejestracji", true, Enums.EmailType.ConfirmAccount)
+        };
+    }
+}
--- JustCommerce.Backend/src/JustCommerce.Domain/Seeders/LanguageSeed.cs
+++ JustCommerce.Backend/src/JustCommerce.Domain/Seeders/LanguageSeed.cs
@@ -9,8 +9,8 @@ namespace JustCommerce.Domain.Seeders
 
         private static IEnumerable<LanguageEntity> SeedLanguage => new List<LanguageEntity>()
         {
-            new LanguageEntity("b80fe531-4bb7-4f2d-a69e-86d8e20602e9", "Poland", "PL-pl"),
-            new LanguageEntity("87f0f759-323f-477d-886a-0afd7272ccd9", "English", "EN-en"),
+            new LanguageEntity("b80fe531-4bb7-4f2d-a69e-86d8e20602e9", "Poland", "PL-pl", true),
+            new LanguageEntity("87f0f759-323f-477d-886a-0afd7272ccd9", "English", "EN-en", false),
         };
     }
 }
