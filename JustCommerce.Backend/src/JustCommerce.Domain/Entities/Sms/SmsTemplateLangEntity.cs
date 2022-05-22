e3fcceac798b9842d3acae6557732be08ccb87b3
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-22 22:12:23 +0200

    Add SmsTemplateLangEntity

---
+++ JustCommerce.Backend/src/JustCommerce.Domain/Entities/Sms/SmsTemplateLangEntity.cs
@@ -0,0 +1,25 @@
+﻿using JustCommerce.Domain.Entities.Language;
+
+namespace JustCommerce.Domain.Entities.Sms
+{
+    public sealed class SmsTemplateLangEntity
+    {
+        public Guid SmsTemplateId { get; set; }
+        public SmsTemplateEntity SmsTemplate { get; set; }
+        public Guid LanguageId { get; set; }
+        public LanguageEntity Language { get; set; }
+        public string Text { get; set; }
+
+        public SmsTemplateLangEntity()
+        {
+
+        }
+
+        public SmsTemplateLangEntity(string smsTemplateId, string languageId, string text)
+        {
+            SmsTemplateId = Guid.Parse(smsTemplateId);
+            LanguageId = Guid.Parse(languageId);
+            Text = text;
+        }
+    }
+}
