c79bc9659752a2265058a4d584e544c1a5c2a21f
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-22 18:11:56 +0200

    Add SmsTemplateLangEntityFacotry

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Factories/EntitiesFactories/Sms/SmsTemplateLangEntityFacotry.cs
@@ -0,0 +1,18 @@
+﻿//using JustCommerce.Application.Common.DTOs.Sms;
+//using JustCommerce.Domain.Entities.Sms;
+
+//namespace JustCommerce.Application.Common.Factories.EntitiesFactories.Sms
+//{
+//    public static class SmsTemplateLangEntityFacotry
+//    {
+//        public static SmsTemplateLangEntity CreateFromDto(SmsTemplateLangDTO dto)
+//        {
+//            return new SmsTemplateLangEntity
+//            {
+//                LanguageId = dto.LanguageId,
+//                SmsTemplateId = dto.SmsTemplateId,
+//                Text = dto.Text,
+//            };
+//        }
+//    }
+//}
