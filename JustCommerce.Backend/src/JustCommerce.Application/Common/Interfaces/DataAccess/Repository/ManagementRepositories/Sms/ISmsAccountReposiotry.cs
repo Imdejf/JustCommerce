e7270dd3f010b7ca04823b9f768acefc9a2ea4c2
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-22 18:12:08 +0200

    Add ISmsAccountReposiotry

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Interfaces/DataAccess/Repository/ManagementRepositories/Sms/ISmsAccountReposiotry.cs
@@ -0,0 +1,9 @@
+﻿using JustCommerce.Application.Common.DataAccess.Repository;
+using JustCommerce.Domain.Entities.Sms;
+
+namespace JustCommerce.Application.Common.Interfaces.DataAccess.Repository.ManagementRepositories.Sms
+{
+    public interface ISmsAccountReposiotry : IBaseRepository<SmsAccountEntity>
+    { 
+    }
+}
