33de242114c5789990dd0b421b8cb1f63df7564a
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-22 18:11:01 +0200

    Remove dictionary

--- JustCommerce.Backend/src/JustCommerce.Application/Common/DTOs/Dictionary/DictionaryLangDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JustCommerce.Application.Common.DTOs.Dictionary
-{
-    public class DictionaryLangDTO
-    {
-        public Guid DictionaryId { get; set; }
-        public Guid LanguageId { get; set; }
-        public string Text { get; set; }
-    }
-}
