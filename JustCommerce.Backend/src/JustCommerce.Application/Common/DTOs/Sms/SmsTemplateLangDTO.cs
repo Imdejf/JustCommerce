0047142e5081b4f650623fb009ea7fadd16ddb99
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-22 18:11:21 +0200

    Add SmsTemplateLangDTO

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/DTOs/Sms/SmsTemplateLangDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace JustCommerce.Application.Common.DTOs.Sms
+{
+    public class SmsTemplateLangDTO
+    {
+        public Guid SmsTemplateId { get; set; }
+        public Guid LanguageId { get; set; }
+        public string Text { get; set; }
+    }
+}
