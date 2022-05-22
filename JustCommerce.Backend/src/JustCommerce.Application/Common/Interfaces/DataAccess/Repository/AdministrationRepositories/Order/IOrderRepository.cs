cce8705d827c19a511a5d0d38da331c339ab099e
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-22 18:12:02 +0200

    Add IOrderRepository

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Interfaces/DataAccess/Repository/AdministrationRepositories/Order/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using JustCommerce.Application.Common.DataAccess.Repository;
+using JustCommerce.Domain.Entities.Order;
+using JustCommerce.Domain.Enums;
+
+namespace JustCommerce.Application.Common.Interfaces.DataAccess.Repository.AdministrationRepositories.Order
+{
+    public interface IOrderRepository : IBaseRepository<OrderEntity>
+    {
+        void SetOrderStatus(Guid orderId, OrderStatus orderStatus);
+        Task<OrderEntity> GetFullyObject(Guid orderId, CancellationToken cancellationToken = default);
+    }
+}
