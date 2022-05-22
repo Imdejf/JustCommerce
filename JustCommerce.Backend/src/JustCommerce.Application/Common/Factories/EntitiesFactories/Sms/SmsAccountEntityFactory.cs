a1bbc63ad0ed3713bc3934433883f99db6f0d87e
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-22 18:11:44 +0200

    Add SmsAccountEntityFactory

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Factories/EntitiesFactories/Sms/SmsAccountEntityFactory.cs
@@ -0,0 +1,26 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace JustCommerce.Application.Common.Factories.EntitiesFactories.Sms
+//{
+//    public static class SmsAccountEntityFactory
+//    {
+//        public static SmsAccountEntity CreateFromDto(SmsAccountDTO dto)
+//        {
+//            return new SmsAccountEntity
+//            {
+//                Description = prodcutLangsDTO.Description,
+//                ImageDescription = prodcutLangsDTO.ImageDescription,
+//                Keywords = prodcutLangsDTO.Keywords,
+//                MetaDescription = prodcutLangsDTO.MetaDescription,
+//                MetaTitle = prodcutLangsDTO.MetaTitle,
+//                Synonyms = prodcutLangsDTO.Synonyms,
+//                Tags = prodcutLangsDTO.Tags,
+//                Name = prodcutLangsDTO.Name,
+//            };
+//        }
+//    }
+//}
