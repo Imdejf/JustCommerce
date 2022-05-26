3b0562c5dab79c8f5c6094f20b28bef7e43447c3
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-26 11:25:58 +0200

    Create new migration Migration_Add_Order_Item_Table

---
+++ JustCommerce.Backend/src/JustCommerce.Persistence/Migrations/20220526091223_Migration_Add_Order_Item_Table.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JustCommerce.Persistence.Migrations
+{
+    public partial class Migration_Add_Order_Item_Table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ConfirmOrder",
+                table: "Order",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "OrderItem",
+                columns: table => new
+                {
+                    OrderId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProductSellableId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    NettoPrice = table.Column<decimal>(type: "decimal", nullable: false),
+                    Tax = table.Column<decimal>(type: "decimal", nullable: false),
+                    GrossPrice = table.Column<decimal>(type: "decimal", nullable: false),
+                    ProducentPrice = table.Column<decimal>(type: "decimal", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderItem", x => new { x.ProductSellableId, x.OrderId });
+                    table.ForeignKey(
+                        name: "FK_OrderItem_Order_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Order",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderItem_ProductSellable_ProductSellableId",
+                        column: x => x.ProductSellableId,
+                        principalTable: "ProductSellable",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItem_OrderId",
+                table: "OrderItem",
+                column: "OrderId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderItem");
+
+            migrationBuilder.DropColumn(
+                name: "ConfirmOrder",
+                table: "Order");
+        }
+    }
+}
