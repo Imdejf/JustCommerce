e6e262e9c601c364a4de53a4a2ccf249dd7f1fc4
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-30 14:16:05 +0200

    Add SubscribedUserEntity

---
+++ JustCommerce.Backend/src/JustCommerce.Domain/Entities/Notification/SubscribedUserEntity.cs
@@ -0,0 +1,13 @@
+﻿using JustCommerce.Domain.Entities.Abstract;
+using JustCommerce.Domain.Entities.Identity;
+using JustCommerce.Domain.Enums;
+
+namespace JustCommerce.Domain.Entities.Notification
+{
+    public sealed class SubscribedUserEntity : AuditableEntity
+    {
+        public Guid UserId { get; set; }
+        public UserEntity User { get; set; }
+        public NotificationType NotificationType { get; set; }
+    }
+}
