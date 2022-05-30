5ef8fc674941d3ab3a36a295a6713d14c082029d
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-30 16:06:18 +0200

    Add EventConsumerWrapper

---
+++ JustCommerce.Backend/src/JustCommerce.Infrastructure/Implementations/Common/MessageBus/MessageBusClient.cs
@@ -0,0 +1,21 @@
+﻿using JustCommerce.Application.Common.Interfaces.CommonServices.MessageBus;
+using JustCommerce.Application.Models.MessageBus.Abstract;
+using RawRabbit;
+
+namespace JustCommerce.Infrastructure.Implementations.Common.MessageBus
+{
+    public sealed class MessageBusClient : IMessageBusClient
+    {
+        private readonly IBusClient _busClient;
+        public MessageBusClient(IBusClient busClient)
+        {
+            _busClient = busClient;
+        }
+
+        public Task PublishAsync<TEvent>(TEvent @event, CancellationToken cancellationToken)
+            where TEvent : BaseNotificationEvent
+        {
+            return _busClient.PublishAsync(@event, token: cancellationToken);
+        }
+    }
+}
