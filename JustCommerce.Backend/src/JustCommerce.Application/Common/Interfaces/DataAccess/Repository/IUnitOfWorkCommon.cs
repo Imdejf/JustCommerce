fd9376e1c8e9a1a8be9ca74063e5766048e73049
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-30 14:26:16 +0200

    Add IUnitOfWorkCommon

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Interfaces/DataAccess/Repository/IUnitOfWorkCommon.cs
@@ -0,0 +1,13 @@
+﻿using JustCommerce.Application.Common.Interfaces.DataAccess.Repository.CommonRepositories.Notification;
+
+namespace JustCommerce.Application.Common.Interfaces.DataAccess.Repository
+{
+    public interface IUnitOfWorkCommon
+    {
+        INotificationRepository Notification { get; }
+        IUserNotificationRepository UserNotification { get; }
+        IUserSubscriberRepository UserSubscriber { get; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
+
