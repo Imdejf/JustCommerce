d77d3471e797eb2e0e2cb0d7e686cfe0d14a6b93
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-30 14:46:15 +0200

    Add GetLatestNotificationPageByUserId with implementation

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Features/CommonFeatures/Notification/Query/GetLatestNotificationPageByUserId.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using JustCommerce.Application.Common.DataAccess.Repository;
+using JustCommerce.Application.Common.Interfaces;
+using JustCommerce.Domain.Entities.Notification;
+
+namespace JustCommerce.Application.Features.CommonFeatures.Notification.Query
+{
+    public static class GetLatestNotificationPageByUserId
+    {
+        public sealed record Query(Guid UserId, int PageSize = 30, int PageNumber = 1) : IRequestWrapper<List<UserNotificationEntity>>;
+
+        public sealed class Handler : IRequestHandlerWrapper<Query, List<UserNotificationEntity>>
+        {
+            private readonly IUnitOfWorkCommon _unitOfWorkCommon;
+            public Handler(IUnitOfWorkCommon unitOfWorkCommon)
+            {
+                _unitOfWorkCommon = unitOfWorkCommon;
+            }
+
+            public Task<List<UserNotificationEntity>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return _unitOfWorkCommon.UserNotification.GetLatestNotificationAsync(request.UserId, request.PageNumber, request.PageSize, cancellationToken);
+            }
+        }
+
+        public sealed class Validator : AbstractValidator<Query>
+        {
+            public Validator()
+            {
+                RuleFor(c => c.UserId).NotEqual(Guid.Empty);
+            }
+        }
+    }
+}
