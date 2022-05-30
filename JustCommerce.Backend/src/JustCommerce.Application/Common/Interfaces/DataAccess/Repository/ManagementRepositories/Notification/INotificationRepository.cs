c016f23a61fe13d03375a900a288dbe94529e23d
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-30 14:15:22 +0200

    Add NotificationRepository with implementation

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Interfaces/DataAccess/Repository/ManagementRepositories/Notification/INotificationRepository.cs
@@ -0,0 +1,9 @@
+﻿using JustCommerce.Domain.Enums;
+
+namespace JustCommerce.Application.Common.Interfaces.DataAccess.Repository.ManagementRepositories.Notification
+{
+    public interface INotificationRepository
+    {
+        Task<bool> ExistsAsync(NotificationType type, CancellationToken cancellationToken = default);
+    }
+}
---
+++ JustCommerce.Backend/src/JustCommerce.Persistence/DataAccess/Repositories/ManagementRepositories/Notification/NotificationRepository.cs
@@ -0,0 +1,24 @@
+﻿using JustCommerce.Application.Common.Interfaces.DataAccess.Repository.ManagementRepositories.Notification;
+using JustCommerce.Domain.Entities.Notification;
+using JustCommerce.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustCommerce.Persistence.DataAccess.Repositories.ManagementRepositories.Notification
+{
+    internal sealed class NotificationRepository : BaseRepository<NotificationEntity>, INotificationRepository
+    {
+        public NotificationRepository(DbSet<NotificationEntity> entity) : base(entity)
+        {
+        }
+
+        public Task<bool> ExistsAsync(NotificationType type, CancellationToken cancellationToken)
+        {
+            return _entity.AnyAsync(c => c.Type == type, cancellationToken);
+        }
+    }
+}
