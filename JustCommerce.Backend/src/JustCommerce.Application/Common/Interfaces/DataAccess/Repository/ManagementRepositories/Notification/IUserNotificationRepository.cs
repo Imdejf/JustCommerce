9c21b33b044aa0ba54c7494a799c8b4e8bdc53c0
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-30 14:15:35 +0200

    Add UserNotificationRepository with implementation

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Common/Interfaces/DataAccess/Repository/ManagementRepositories/Notification/IUserNotificationRepository.cs
@@ -0,0 +1,13 @@
+﻿using JustCommerce.Application.Common.DataAccess.Repository;
+using JustCommerce.Domain.Entities.Notification;
+
+namespace JustCommerce.Application.Common.Interfaces.DataAccess.Repository.ManagementRepositories.Notification
+{
+    public interface IUserNotificationRepository : IBaseRepository<UserNotificationEntity>
+    {
+        Task<List<UserNotificationEntity>> GetLatestNotificationAsync(Guid userId, int pageNumber, int pageSize, CancellationToken cancellationToken);
+        Task<bool> IsBindedWithUserAsync(Guid userNotificationId, Guid userId, CancellationToken cancellationToken);
+        void Complete(Guid sendNotificationId);
+        void See(Guid sendNotificationId);
+    }
+}
---
+++ JustCommerce.Backend/src/JustCommerce.Persistence/DataAccess/Repositories/ManagementRepositories/Notification/UserNotificationRepository.cs
@@ -0,0 +1,43 @@
+﻿using JustCommerce.Application.Common.Interfaces.DataAccess.Repository.ManagementRepositories.Notification;
+using JustCommerce.Domain.Entities.Notification;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustCommerce.Persistence.DataAccess.Repositories.ManagementRepositories.Notification
+{
+    internal sealed class UserNotificationRepository : BaseRepository<UserNotificationEntity>, IUserNotificationRepository
+    {
+        public UserNotificationRepository(DbSet<UserNotificationEntity> entity) : base(entity)
+        {
+        }
+
+        public void Complete(Guid sendNotificationId)
+        {
+            var attachedEntity = _entity.Attach(new UserNotificationEntity { Id = sendNotificationId });
+            attachedEntity.Entity.Completed = true;
+        }
+
+        public Task<bool> IsBindedWithUserAsync(Guid userNotificationId, Guid userId, CancellationToken cancellationToken)
+        {
+            return _entity.AnyAsync(c => c.Id == userNotificationId && c.UserId == userId, cancellationToken);
+        }
+
+        public void See(Guid sendNotificationId)
+        {
+            var attachedEntity = _entity.Attach(new UserNotificationEntity { Id = sendNotificationId });
+            attachedEntity.Entity.Seen = true;
+        }
+
+        public Task<List<UserNotificationEntity>> GetLatestNotificationAsync(Guid userId, int pageNumber, int pageSize, CancellationToken cancellationToken)
+        {
+            return _entity.OrderBy(c => c.CreationDate)
+                          .Where(c => c.UserId == userId)
+                          .Skip((pageNumber - 1) * pageSize)
+                          .Take(pageSize)
+                          .ToListAsync(cancellationToken);
+        }
+    }
