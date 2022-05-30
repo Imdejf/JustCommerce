4dc29f71de367b4e89bd6a357ab66fad5b71fc87
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-30 14:37:32 +0200

    Add implementation CompleteNotification

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Features/CommonFeatures/Notification/Command/CompleteNotification.cs
@@ -0,0 +1,50 @@
+﻿using FluentValidation;
+using JustCommerce.Application.Common.DataAccess.Repository;
+using JustCommerce.Application.Common.Interfaces;
+using JustCommerce.Shared.Exceptions;
+using MediatR;
+
+namespace JustCommerce.Application.Features.CommonFeatures.Notification.Command
+{
+    public static class CompleteNotification
+    {
+        public sealed record Command(Guid SendNotificationId, Guid UserId) : IRequestWrapper<Unit>;
+
+        public sealed class Handler : IRequestHandlerWrapper<Command, Unit>
+        {
+            private readonly IUnitOfWorkCommon _unitOfWorkCommon;
+
+            public Handler(IUnitOfWorkCommon unitOfWorkCommon)
+            {
+                _unitOfWorkCommon = unitOfWorkCommon;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                if (!await _unitOfWorkCommon.UserNotification.ExistsAsync(request.SendNotificationId, cancellationToken))
+                {
+                    throw new EntityNotFoundException("UserNotification is not exists");
+                }
+
+                if (!await _unitOfWorkCommon.UserNotification.IsBindedWithUserAsync(request.SendNotificationId, request.UserId, cancellationToken))
+                {
+                    throw new InvalidRequestException("Send user id not binded with send SendNotification");
+                }
+
+                _unitOfWorkCommon.UserNotification.Complete(request.SendNotificationId);
+                await _unitOfWorkCommon.SaveChangesAsync(cancellationToken);
+
+                return Unit.Value;
+            }
+        }
+
+        public sealed class Validator : AbstractValidator<Command>
+        {
+            public Validator()
+            {
+                RuleFor(c => c.UserId).NotEqual(Guid.Empty);
+                RuleFor(c => c.SendNotificationId).NotEqual(Guid.Empty);
+            }
+        }
+    }
+}
