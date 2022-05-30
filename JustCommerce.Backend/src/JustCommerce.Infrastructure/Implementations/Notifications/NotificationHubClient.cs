2abf1d0857bb4d94be4565abd3b6e8c6fe00d77f
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-30 16:06:40 +0200

    Add NotificationHubClient

---
+++ JustCommerce.Backend/src/JustCommerce.Infrastructure/Implementations/Notifications/NotificationHubClient.cs
@@ -0,0 +1,23 @@
+﻿using JustCommerce.Application.Common.Interfaces.Notification;
+using JustCommerce.Infrastructure.Hubs.NotificationHubs;
+using Microsoft.AspNetCore.SignalR;
+
+namespace JustCommerce.Infrastructure.Implementations.Notifications
+{
+    internal class NotificationHubClient : INotificationHubClient
+    {
+        private readonly IHubContext<NotificationHub> _hubContext;
+
+        public NotificationHubClient(IHubContext<NotificationHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        public Task SendAsync(Guid userId, string description, CancellationToken cancellationToken)
+        {
+            return _hubContext.Clients.User(userId.ToString())
+                                      .SendAsync("ReceiveMessage", description, cancellationToken);
+
+        }
+    }
+}
