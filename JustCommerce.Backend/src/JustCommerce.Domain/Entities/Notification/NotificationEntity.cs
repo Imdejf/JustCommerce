3a96ef9644fb465c9a2414d02ccfa683d891d4af
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-30 14:16:01 +0200

    Add NotificationEntity

---
+++ JustCommerce.Backend/src/JustCommerce.Domain/Entities/Notification/NotificationEntity.cs
@@ -0,0 +1,10 @@
+﻿using JustCommerce.Domain.Entities.Abstract;
+using JustCommerce.Domain.Enums;
+
+namespace JustCommerce.Domain.Entities.Notification
+{
+    public sealed class NotificationEntity : AuditableEntity
+    {
+        public NotificationType NotificationType { get; set; }
+    }
+}
