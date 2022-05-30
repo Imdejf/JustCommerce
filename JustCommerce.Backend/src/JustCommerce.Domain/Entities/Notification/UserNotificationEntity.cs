f20b8f507ddbd2c427f238b4b92dd48947843afb
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-30 14:16:10 +0200

    Add UserNotificationEntity

---
+++ JustCommerce.Backend/src/JustCommerce.Domain/Entities/Notification/UserNotificationEntity.cs
@@ -0,0 +1,16 @@
+﻿using JustCommerce.Domain.Entities.Abstract;
+using JustCommerce.Domain.Entities.Identity;
+using JustCommerce.Domain.Enums;
+
+namespace JustCommerce.Domain.Entities.Notification
+{
+    public sealed class UserNotificationEntity : AuditableEntity
+    {
+        public Guid UserId { get; set; }
+        public UserEntity User { get; set; }
+        public NotificationType NotificationType { get; set; }
+        public bool Seen { get; set; }
+        public bool Completed { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
