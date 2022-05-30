497769485f513dd24400b2d28a3338f6d47832dc
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-30 14:37:41 +0200

    Add implmentation SetSeeNotification

---
+++ JustCommerce.Backend/src/JustCommerce.Application/Features/CommonFeatures/Notification/Command/SetSeeNotification.cs
@@ -0,0 +1,49 @@
+﻿using FluentValidation;
+using JustCommerce.Application.Common.DataAccess.Repository;
+using JustCommerce.Shared.Exceptions;
+using MediatR;
+
+namespace JustCommerce.Application.Features.CommonFeatures.Notification.Command
+{
+    internal class SetSeeNotification
+    {
+        public sealed record Command(Guid SendNotificationId, Guid UserId) : IRequest<Unit>;
+
+        public sealed class Handler : IRequestHandler<Command, Unit>
+        {
+            private readonly IUnitOfWorkCommon _unitOfWorkCommon;
+            public Handler(IUnitOfWorkCommon unitOfWork)
+            {
+                _unitOfWorkCommon = unitOfWork;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+
+                if (!await _unitOfWorkCommon.UserNotification.ExistsAsync(request.SendNotificationId, cancellationToken))
+                {
+                    throw new EntityNotFoundException("Send notification no exists");
+                }
+
+                if (!await _unitOfWorkCommon.UserNotification.IsBindedWithUserAsync(request.SendNotificationId, request.UserId, cancellationToken))
+                {
+                    throw new InvalidRequestException($"Notification with Id : {request.SendNotificationId} is not binded with User with Id : {request.UserId}");
+                }
+
+                _unitOfWorkCommon.UserNotification.See(request.SendNotificationId);
+                await _unitOfWorkCommon.SaveChangesAsync(cancellationToken);
+
+                return Unit.Value;
+            }
+        }
+
+        public sealed class Validator : AbstractValidator<Command>
+        {
+            public Validator()
+            {
+                RuleFor(c => c.UserId).NotEqual(Guid.Empty);
+                RuleFor(c => c.SendNotificationId).NotEqual(Guid.Empty);
+            }
+        }
+    }
+}
