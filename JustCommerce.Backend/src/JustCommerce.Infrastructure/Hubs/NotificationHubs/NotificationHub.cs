e296bbda985b0a8471c2b1d0067aee7b4adc23d8
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-05-30 16:06:11 +0200

    Add NotificationHub

---
+++ JustCommerce.Backend/src/JustCommerce.Infrastructure/Hubs/NotificationHubs/NotificationHub.cs
@@ -0,0 +1,29 @@
+﻿using JustCommerce.Application.Common.Interfaces.Notification;
+using JustCommerce.Infrastructure.Implementations.Common;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace JustCommerce.Infrastructure.Hubs.NotificationHubs
+{
+    [Authorize]
+    public sealed class NotificationHub : Hub<INotificationHubClient>
+    {
+        private readonly UserIdsManager _userIdsManager;
+        public NotificationHub(UserIdsManager userIdsManager)
+        {
+            _userIdsManager = userIdsManager;
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            _userIdsManager.Add(Context.ConnectionId, Context.UserIdentifier);
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            _userIdsManager.RemoveByConnectionId(Context.ConnectionId);
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
