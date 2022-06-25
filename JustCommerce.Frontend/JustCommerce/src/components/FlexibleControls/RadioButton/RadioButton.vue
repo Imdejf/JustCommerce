2a7fa38ce5dbcc3682681d0ea05f8f9e87943351
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-06-25 19:07:40 +0200

    Added radiobutton component

---
+++ JustCommerce.Frontend/JustCommerce/src/components/FlexibleControls/RadioButton/RadioButton.vue
@@ -0,0 +1,55 @@
+<template>
+  <input-wrapper ref="wrapper" :config="wrapperConfig" :class="'dx-input-wrapper--radio'">
+    <dx-input
+      :value="radioValue"
+      @valueChanged="radioValueChanged"
+      :width="config.width"
+      :height="config.height"
+      :readOnly="config.readOnly"
+      :dataSource="config.options"
+      :layout="config.layout"
+      class="pl-2"
+    >
+    </dx-input>
+  </input-wrapper>
+</template>
+
+<script>
+import ControlMixin from '../ControlMixin'
+import InputWrapper from '../InputWrapper.vue'
+import DxInput from 'devextreme-vue/radio-group'
+
+export default {
+  mixins: [ControlMixin],
+  components: {
+    InputWrapper,
+    DxInput
+  },
+  props: {
+    value: {
+      type: String,
+      required: false
+    },
+    options: {
+      type: Array,
+      required: true
+    },
+    useHorizontalLayout: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+  computed: {
+    radioValue: function () {
+      return this.options.find((e) => e.value === this.value)
+    }
+  },
+  methods: {
+    radioValueChanged: function (event) {
+      const value = event.value.value
+      this.onValueChanged({ value: `${value}` })
+    }
+  }
+}
+</script>
